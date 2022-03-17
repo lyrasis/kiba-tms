eb440d21193972674a2778a53ad76b670748a15f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-03-17 14:07:31 -0400

    add fingerprint field to compiled names

--- lib/kiba/tms/jobs/in_between/name_compilation.rb
+++ lib/kiba/tms/jobs/in_between/name_compilation.rb
@@ -10,7 +10,7 @@ module Kiba
 
           def from_constituents
             xforms = Kiba.job_segment do              
-              transform Delete::Fields, fields: %i[namedata constituentid defaultdisplaybioid defaultnameid]
+              transform Delete::Fields, fields: %i[namedata defaultdisplaybioid defaultnameid displaydate]
               unless Kiba::Tms.constituents.date_append.to_types == [:none]
                 transform Kiba::Tms::Transforms::Constituents::AppendDatesToNames
               end
@@ -63,9 +63,14 @@ module Kiba
             xforms = Kiba.job_segment do
               @alphasorter = Tms::Services::Constituents::PersonNameAlphasortConstructor.new
               @displaynamer = Tms::Services::Constituents::PersonDisplaynameConstructor.new
+              @fields = %i[constituenttype lastname firstname nametitle middlename suffix salutation nationality culturegroup]
               
-              transform Delete::FieldsExcept,
-                fields: %i[constituenttype lastname firstname nametitle middlename suffix salutation nationality culturegroup]
+              transform Delete::FieldsExcept, fields: @fields
+              
+              transform CombineValues::FromFieldsWithDelimiter, sources: @fields, target: :combined,
+                sep: ' - ', delete_sources: false
+              transform Deduplicate::Table, field: :combined, delete_field: true
+
               transform Tms::Transforms::Constituents::KeepOrgsWithPersonNameParts
               transform do |row|
                 row[:alphasort] = @alphasorter.call(row)
@@ -76,6 +81,7 @@ module Kiba
                 row[:displayname] = @displaynamer.call(row)
                 row
               end
+              
               transform Merge::ConstantValue, target: :constituenttype, value: 'Person'
               transform Merge::ConstantValue, target: :termsource, value: 'TMS Constituents.orgs_with_person_names'
               transform Cspace::NormalizeForID, source: Tms.constituents.preferred_name_field, target: :norm
@@ -93,18 +99,7 @@ module Kiba
 
 
           def from_loans
-            Kiba::Extend::Jobs::MultiSourcePrepJob.new(
-              files: {
-                source: :prep__loans,
-                destination: :names__from_loans
-              },
-              transformer: from_loans_xforms,
-              helper: Kiba::Tms.name_compilation.multi_source_normalizer
-            )
-          end
-
-          def from_loans_xforms
-            Kiba.job_segment do
+            xforms = Kiba.job_segment do
               transform Delete::FieldsExcept, fields: %i[approvedby contact requestedby]
               transform CombineValues::FromFieldsWithDelimiter,
                 sources: %i[approvedby contact requestedby], target: :combined,
@@ -117,21 +112,19 @@ module Kiba
               transform Rename::Field, from: :combined, to: Tms.constituents.preferred_name_field
               transform Merge::ConstantValue, target: :termsource, value: 'TMS Loans'
             end
-          end
-
-          def from_obj_accession
+            
             Kiba::Extend::Jobs::MultiSourcePrepJob.new(
               files: {
-                source: :prep__obj_accession,
-                destination: :names__from_obj_accession
+                source: :prep__loans,
+                destination: :names__from_loans
               },
-              transformer: from_obj_accession_xforms,
+              transformer: xforms,
               helper: Kiba::Tms.name_compilation.multi_source_normalizer
             )
           end
 
-          def from_obj_accession_xforms
-            Kiba.job_segment do
+          def from_obj_accession
+            xforms = Kiba.job_segment do
               transform Delete::FieldsExcept, fields: %i[authorizer initiator]
               transform CombineValues::FromFieldsWithDelimiter,
                 sources: %i[authorizer initiator], target: :combined,
@@ -144,21 +137,19 @@ module Kiba
               transform Rename::Field, from: :combined, to: Tms.constituents.preferred_name_field
               transform Merge::ConstantValue, target: :termsource, value: 'TMS Objaccession'
             end
-          end
-
-          def from_obj_locations
+            
             Kiba::Extend::Jobs::MultiSourcePrepJob.new(
               files: {
-                source: :prep__obj_locations,
-                destination: :names__from_obj_locations
+                source: :prep__obj_accession,
+                destination: :names__from_obj_accession
               },
-              transformer: from_obj_locations_xforms,
+              transformer: xforms,
               helper: Kiba::Tms.name_compilation.multi_source_normalizer
             )
           end
 
-          def from_obj_locations_xforms
-            Kiba.job_segment do
+          def from_obj_locations
+            xforms = Kiba.job_segment do
               transform Delete::FieldsExcept, fields: %i[approver handler requestedby]
               transform CombineValues::FromFieldsWithDelimiter,
                 sources: %i[approver handler requestedby], target: :combined,
@@ -171,14 +162,29 @@ module Kiba
               transform Rename::Field, from: :combined, to: Tms.constituents.preferred_name_field
               transform Merge::ConstantValue, target: :termsource, value: 'TMS Obj_Locations'
             end
+
+            Kiba::Extend::Jobs::MultiSourcePrepJob.new(
+              files: {
+                source: :prep__obj_locations,
+                destination: :names__from_obj_locations
+              },
+              transformer: xforms,
+              helper: Kiba::Tms.name_compilation.multi_source_normalizer
+            )
           end
 
           def initial_compile
             xforms = Kiba.job_segment do
               @deduper = {}
+
+              transform Tms::Transforms::AddFingerprint,
+                fields: Tms::Services::FingerprintFields.names,
+                delim: '|||',
+                target: :fingerprint
               transform Deduplicate::Flag, on_field: :norm, in_field: :duplicate, using: @deduper,
                 explicit_no: false
               transform Append::NilFields, fields: Tms.name_compilation.multi_source_normalizer.get_fields
+              transform Append::NilFields, fields: %i[migration_action approx_normalized]
             end
             
             Kiba::Extend::Jobs::Job.new(
@@ -214,19 +220,19 @@ module Kiba
               transform Tms::Transforms::Constituents::CleanPersonNamePartsFromOrg
               transform Tms::Transforms::Constituents::CleanOrgNameInfoFromPerson
               transform Tms::Transforms::Constituents::FlagPersonNamesLackingNameDetails
+
+              # This marks all rows with duplicate values as duplicates, not just subsequent rows
               transform Merge::MultiRowLookup,
                 fieldmap: {normnew: :norm},
                 lookup: names__flagged_duplicates,
                 keycolumn: :norm,
                 constantmap: {duplicate: 'y'}
-              transform Delete::Fields, fields: :normnew
+              transform Delete::Fields, fields: %i[normnew constituentid]
 
               transform Rename::Field, from: Kiba::Tms.constituents.preferred_name_field, to: :preferred_name_form
               transform Rename::Field, from: Kiba::Tms.constituents.alt_name_field, to: :variant_name_form
               transform Rename::Field, from: :norm, to: :normalized_form
-              transform Copy::Field, from: :preferred_name_form, to: :use_name_form
-              transform Copy::Field, from: :constituenttype, to: :use_type
-              transform Append::NilFields, fields: %i[use_variant_forms]
+              
             end
             
             Kiba::Extend::Jobs::Job.new(
