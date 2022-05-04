6b28932332680be609a340fc80bff43ba2352a21
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-04 16:33:05 -0400

    separate emailtype and webtype values

--- lib/kiba/tms/transforms/con_email/separate_email_and_nonemail.rb
+++ lib/kiba/tms/transforms/con_email/separate_email_and_nonemail.rb
@@ -11,12 +11,25 @@ module Kiba
           def process(row)
             row[:email] = nil
             row[:web] = nil
+            row[:webtype] = nil
             val = row[:emailaddress]
             return row if val.blank?
 
-            val['@'] ? row[:email] = val : row[:web] = val
+            val['@'] ? treat_as_email(val, row) : treat_as_web(val, row)
             row.delete(:emailaddress)
             row
+          end
+
+          private
+
+          def treat_as_email(val, row)
+            row[:email] = val
+          end
+
+          def treat_as_web(val, row)
+            row[:web] = val
+            row[:webtype] = row[:emailtype]
+            row[:emailtype] = nil
           end
         end
       end
