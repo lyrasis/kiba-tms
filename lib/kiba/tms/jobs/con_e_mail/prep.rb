04542a5428e8c98328bcfd0c6c0aa4e28f602855
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-04 16:42:33 -0400

    also clear web values out of description

--- lib/kiba/tms/jobs/con_e_mail/prep.rb
+++ lib/kiba/tms/jobs/con_e_mail/prep.rb
@@ -21,10 +21,12 @@ module Kiba
           def xforms
             Kiba.job_segment do
               transform Tms::Transforms::DeleteTmsFields
+              # merge in email type
               transform Merge::MultiRowLookup,
                 lookup: prep__email_types,
                 keycolumn: :emailtypeid,
                 fieldmap: { emailtype: :emailtype }
+
               transform Merge::MultiRowLookup,
                 lookup: nameclean__by_constituentid,
                 keycolumn: :constituentid,
@@ -36,11 +38,14 @@ module Kiba
               transform Tms::Transforms::ConEmail::AddRetentionFlag
               transform Tms::Transforms::ConEmail::SeparateEmailAndNonemail
 
+              # clear "email" and "web" values out of description
               transform do |row|
                 desc = row[:description]
                 next row if desc.blank?
 
                 if desc =~ /^email$/i
+                  row[:description] = nil
+                elsif desc =~ /^web$/i
                   row[:description] = nil
                 end
                 row
