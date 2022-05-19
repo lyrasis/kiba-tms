a1938bbb27b7bbe88db8c01c66c3f46023d0ff11
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-05-19 10:19:17 -0400

    split thes_xref_types into separate module files

---
+++ lib/kiba/tms/jobs/thes_xref_types/prep.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module ThesXrefTypes
+        module Prep
+          module_function
+          
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :tms__thes_xref_types,
+                destination: :prep__thes_xref_types
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Tms::Transforms::DeleteTmsFields
+              transform Delete::Fields, fields: %i[multiselect archivedeletes showguideterms broadesttermfirst
+                                                   numlevels alwaysdisplayfullpath]
+              transform FilterRows::FieldMatchRegexp,
+                action: :reject,
+                field: :thesxreftype,
+                match: '^\(not assigned\)$',
+                ignore_case: true
+            end
+          end
+        end
+      end
+    end
+  end
+end
