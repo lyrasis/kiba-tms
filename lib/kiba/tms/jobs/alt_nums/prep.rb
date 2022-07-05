d633c986bac1bc4e137f4d86fcda717290983566
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-05 14:18:46 -0400

    merge human readable target record numbers/terms for use in reporting

--- lib/kiba/tms/jobs/alt_nums/prep.rb
+++ lib/kiba/tms/jobs/alt_nums/prep.rb
@@ -11,7 +11,11 @@ module Kiba
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :tms__alt_nums,
-                destination: :prep__alt_nums
+                destination: :prep__alt_nums,
+                lookup: %i[
+                           tms__constituents
+                           tms__objects
+                           ]
               },
               transformer: xforms
             )
@@ -28,6 +32,29 @@ module Kiba
               transform Clean::RegexpFindReplaceFieldVals, fields: :description, find: '\\\\n', replace: ''
               transform Clean::RegexpFindReplaceFieldVals, fields: :all, find: '^(%CR%%LF%)+', replace: ''
               transform Clean::RegexpFindReplaceFieldVals, fields: :all, find: '(%CR%%LF%)+$', replace: ''
+              transform Merge::MultiRowLookup,
+                lookup: tms__constituents,
+                keycolumn: :recordid,
+                fieldmap: {constituent: Tms.constituents.preferred_name_field},
+                conditions: ->(origrow, mergerows) do
+                  return [] unless origrow[:tablename] == 'Constituents'
+                  
+                  mergerows
+                end
+              transform Merge::MultiRowLookup,
+                lookup: tms__objects,
+                keycolumn: :recordid,
+                fieldmap: {object: :objectnumber},
+                conditions: ->(origrow, mergerows) do
+                  return [] unless origrow[:tablename] == 'Objects'
+                  
+                  mergerows
+                end
+              transform CombineValues::FromFieldsWithDelimiter,
+                sources: %i[constituent object],
+                target: :targetrecord,
+                sep: '',
+                delete_sources: true
             end
           end
         end
