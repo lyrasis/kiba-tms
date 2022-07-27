2900b4d89301cef6a50f745208bc7d6a6637086b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-07-27 18:15:31 -0400

    make compiled name sources conditional

--- lib/kiba/tms/jobs/names/compiled_data_raw.rb
+++ lib/kiba/tms/jobs/names/compiled_data_raw.rb
@@ -10,21 +10,25 @@ module Kiba
           def job
             Kiba::Extend::Jobs::Job.new(
               files: {
-                source: %i[
-                           names__from_constituents
-                           names__from_constituents_orgs_from_persons
-                           names__from_constituents_persons_from_orgs
-                           names__from_loans
-                           names__from_obj_accession
-                           names__from_obj_locations
-                           names__from_assoc_parents_for_con
-                          ],
+                source: sources,
                 destination: :names__initial_compile
               },
               transformer: xforms
             )
           end
 
+          def sources
+            base = %i[
+                      names__from_constituents
+                      names__from_constituents_orgs_from_persons
+                      names__from_constituents_persons_from_orgs
+                     ]
+            base << :names__from_loans if Tms::Loans.used
+            base << :names__from_obj_accession if Tms::ObjAccession.used
+            base << :names__from_obj_locations if Tms::ObjLocations.used
+            base << :names__from_assoc_parents_for_con if Tms::AssocParents.used && Tms::AssocParents.for_constituents 
+            base
+          end
           def xforms
             Kiba.job_segment do
               @deduper = {}
