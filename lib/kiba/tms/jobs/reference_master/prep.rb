a0d203d8ca3e933af357506264ad87c4d5be37cd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-01 18:01:31 -0400

    conditionally merge text entries into reference master via custom transform

--- lib/kiba/tms/jobs/reference_master/prep.rb
+++ lib/kiba/tms/jobs/reference_master/prep.rb
@@ -29,6 +29,7 @@ module Kiba
 
           def xforms
             Kiba.job_segment do
+              transform Tms::Transforms::DeleteTmsFields
               transform Delete::Fields, fields: Tms::ReferenceMaster.delete_fields
               transform Merge::MultiRowLookup,
                 lookup: prep__ref_formats,
@@ -41,11 +42,16 @@ module Kiba
                 fieldmap: {language: :language}
               transform Delete::Fields, fields: :languageid
 
-              if Tms::TextEntries.target_tables.any?('ReferenceMaster')
-                transform Merge::MultiRowLookup,
-                  lookup: text_entries__for_reference_master,
-                  keycolumn: :referenceid,
-                  fieldmap: {termfullcitation: :textentry}
+              if Tms::TextEntries.for?('ReferenceMaster')
+                if Tms::ReferenceMaster.text_entry_merger
+                  Tms::ReferenceMaster.config.text_entry_lookup = text_entries__for_reference_master
+                  transform Tms::ReferenceMaster.text_entry_merger
+                else
+                  transform Merge::MultiRowLookup,
+                    lookup: text_entries__for_reference_master,
+                    keycolumn: :referenceid,
+                    fieldmap: {termfullcitation: :textentry}
+                end
               end
             end
           end
--- lib/kiba/tms/reference_master.rb
+++ lib/kiba/tms/reference_master.rb
@@ -12,6 +12,10 @@ module Kiba
       setting :used, default: ->{ Tms::Tables::List.call.any?('ReferenceMaster') }, reader: true
       # Fields beyond DeleteTmsFields general fields to delete
       setting :delete_fields, default: %i[alphaheading sortnumber publicaccess conservationentityid], reader: true
+      # Used to pass text entry lookup to text_entry_merger if applicable
+      setting :text_entry_lookup, default: {}, reader: true
+      # Custom transform to merge in text entries
+      setting :text_entry_merger, default: nil, reader: true
     end
   end
 end
