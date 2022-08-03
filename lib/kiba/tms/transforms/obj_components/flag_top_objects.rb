65f3d62dbd5fb961f3bd1e6c06f76ae00d107a0f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-02 20:42:28 -0400

    replace deprecated Helpers.field_values

--- lib/kiba/tms/transforms/obj_components/flag_top_objects.rb
+++ lib/kiba/tms/transforms/obj_components/flag_top_objects.rb
@@ -8,13 +8,15 @@ module Kiba
           include Kiba::Extend::Transforms::Helpers
 
           def initialize
+            @fields = %i[parentobjectnumber componentnumber]
+            @getter = Kiba::Extend::Transforms::Helpers::FieldValueGetter.new(fields: fields)
             @target = :is_top_object
           end
 
           def process(row)
             row[target] = nil
 
-            nums = field_values(row: row, fields: %i[parentobjectnumber componentnumber]).values.uniq.length
+            nums = getter.call(row).values.uniq.length
             return row if nums > 1
 
             row[target] = 'y'
@@ -23,7 +25,7 @@ module Kiba
 
           private
 
-          attr_reader :target
+          attr_reader :fields, :getter, :target
         end
       end
     end
