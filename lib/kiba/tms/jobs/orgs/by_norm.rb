92a317b249dd77d2946414f38aeb7ff433664750
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-24 15:04:49 -0400

    Persons/OrgsByNorm source depends on cleanup iteration

--- lib/kiba/tms/jobs/orgs/by_norm.rb
+++ lib/kiba/tms/jobs/orgs/by_norm.rb
@@ -7,21 +7,36 @@ module Kiba
         module ByNorm
           module_function
 
-          ITERATION = Tms::Names.cleanup_iteration
-
           def job
             Kiba::Extend::Jobs::Job.new(
               files: {
-                source: "nameclean#{ITERATION}__orgs_kept".to_sym,
+                source: source,
                 destination: :orgs__by_norm
               },
               transformer: xforms
             )
           end
 
+          def source
+            iteration = Tms::Names.cleanup_iteration
+            if iteration
+              "nameclean#{iteration}__orgs_kept".to_sym
+            else
+              :name_compile__unique
+            end
+          end
+          
           def xforms
             Kiba.job_segment do
-              
+              unless Tms::Names.cleanup_iteration
+                pref = Tms::Constituents.preferred_name_field
+                
+                transform FilterRows::WithLambda,
+                  action: :keep,
+                  lambda: ->(row){ row[:contype] == 'Organization' && row[:relation_type] == '_main term' }
+                transform Delete::FieldsExcept, fields: pref
+                transform Kiba::Extend::Transforms::Cspace::NormalizeForID, source: pref, target: :norm
+              end
             end
           end
         end
--- lib/kiba/tms/jobs/persons/by_norm.rb
+++ lib/kiba/tms/jobs/persons/by_norm.rb
@@ -7,20 +7,36 @@ module Kiba
         module ByNorm
           module_function
 
-          ITERATION = Tms::Names.cleanup_iteration
-
           def job
             Kiba::Extend::Jobs::Job.new(
               files: {
-                source: "nameclean#{ITERATION}__persons_kept".to_sym,
+                source: source,
                 destination: :persons__by_norm
               },
               transformer: xforms
             )
           end
 
+          def source
+            iteration = Tms::Names.cleanup_iteration
+            if iteration
+              "nameclean#{iteration}__persons_kept".to_sym
+            else
+              :name_compile__unique
+            end
+          end
+                    
           def xforms
             Kiba.job_segment do
+              unless Tms::Names.cleanup_iteration
+                pref = Tms::Constituents.preferred_name_field
+                
+                transform FilterRows::WithLambda,
+                  action: :keep,
+                  lambda: ->(row){ row[:contype] == 'Person' && row[:relation_type] == '_main term' }
+                transform Delete::FieldsExcept, fields: pref
+                transform Kiba::Extend::Transforms::Cspace::NormalizeForID, source: pref, target: :norm
+              end
             end
           end
         end
