2774c51ce0bf3a6d6939fb8d101dec919b38c095
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-25 17:20:17 -0400

    add LocApprovers::Prep job

---
+++ lib/kiba/tms/jobs/loc_approvers/prep.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module LocApprovers
+        module Prep
+          module_function
+          
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :tms__loc_approvers,
+                destination: :prep__loc_approvers
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Tms::Transforms::DeleteTmsFields
+              transform Tms::Transforms::DeleteNoValueTypes, field: :approver
+            end
+          end
+        end
+      end
+    end
+  end
+end
