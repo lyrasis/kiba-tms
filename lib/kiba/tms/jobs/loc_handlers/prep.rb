267bbdd263cc346916be608e0d6fb9d0df90a9b7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-08-25 17:18:59 -0400

    add LocHandlers::Prep job

---
+++ lib/kiba/tms/jobs/loc_handlers/prep.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module LocHandlers
+        module Prep
+          module_function
+          
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :tms__loc_handlers,
+                destination: :prep__loc_handlers
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform Tms::Transforms::DeleteTmsFields
+              transform Tms::Transforms::DeleteNoValueTypes, field: :handler
+            end
+          end
+        end
+      end
+    end
+  end
+end
