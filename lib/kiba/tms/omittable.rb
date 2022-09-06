9d667de174acb12b788848784f25f864697ea7dd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-06 16:28:00 -0400

    add mixin methods to Omittable

--- lib/kiba/tms/omittable.rb
+++ lib/kiba/tms/omittable.rb
@@ -13,12 +13,21 @@ module Kiba
     # - defind `empty_fields` setting (Array)
     # - `extend Tms::Omittable`
     module Omittable
+      def delete_omitted_fields(hash)
+        omitted_fields.each{ |field| hash.delete(field) if hash.key?(field) }
+        hash
+      end
+      
       def omitted_fields
         ( delete_fields + empty_fields ).uniq
       end
 
       def omitting_fields?
         true unless omitted_fields.empty?
+      end
+
+      def subtract_omitted_fields(arr)
+        arr - omitted_fields
       end
     end
   end
