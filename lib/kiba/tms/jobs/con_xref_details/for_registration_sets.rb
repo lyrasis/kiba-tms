15e4cc9fb82ca88dcac5434599f808070bada40e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-08 18:48:45 -0400

    add ForAccessionLot and ForRegistrationSet jobs to ConXrefDetails

---
+++ lib/kiba/tms/jobs/con_xref_details/for_registration_sets.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module ConXrefDetails
+        module ForRegistrationSets
+          module_function
+
+          def job
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :prep__con_xref_details,
+                destination: :con_xref_details__for_registration_sets
+              },
+              transformer: xforms
+            )
+          end
+
+          def xforms
+            Kiba.job_segment do
+              transform FilterRows::FieldEqualTo, action: :keep, field: :tablename, value: 'RegistrationSets'
+              transform Delete::Fields, fields: :tablename
+            end
+          end
+        end
+      end
+    end
+  end
+end
