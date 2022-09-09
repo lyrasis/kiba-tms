7a0beb603f06978e88b0c34fb80c997a13b4f128
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-08 20:34:54 -0400

    standardize config for Tms::ObjectStatuses

--- lib/kiba/tms/object_statuses.rb
+++ lib/kiba/tms/object_statuses.rb
@@ -6,6 +6,15 @@ module Kiba
   module Tms
     module ObjectStatuses
       extend Dry::Configurable
+      extend Tms::Omittable
+      module_function
+
+      # whether or not table is used
+      setting :used, default: ->{ Tms::Table::List.include?('ObjectStatuses') }, reader: true
+      # Fields beyond DeleteTmsFields general fields to delete
+      setting :delete_fields, default: %i[], reader: true
+      setting :empty_fields, default: %i[], reader: true
+
       # map values in TMS table to InventoryStatuses vocabulary terms
       setting :inventory_status_mapping, default: {}, reader: true
     end
