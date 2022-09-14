6cdf8cbd7614e1e6947cc0cdd411749ec27d2b3b
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-14 16:36:14 -0400

    fix loc approver and handler prep jobs and config

---
+++ lib/kiba/tms/loc_approvers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'dry-configurable'
+
+module Kiba
+  module Tms
+    # Looks like a type lookup table, but nothing looks up from it
+    # Names are extracted from it
+    module LocApprovers
+      extend Dry::Configurable
+      extend Tms::Mixins::AutoConfigurable
+      module_function
+
+      setting :delete_fields, default: %i[], reader: true
+      setting :empty_fields, default: %i[], reader: true
+    end
+  end
+end
