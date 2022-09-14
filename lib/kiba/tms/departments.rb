34b1623b7ceb36a47b6416c8607a0c0a88a9ab91
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-14 16:09:54 -0400

    fix prep__departments

---
+++ lib/kiba/tms/departments.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require 'dry-configurable'
+
+module Kiba
+  module Tms
+    module Departments
+      extend Dry::Configurable
+      extend Tms::Mixins::AutoConfigurable
+      extend Tms::Mixins::TypeLookupTable
+      module_function
+
+      setting :delete_fields,
+        default: %i[mnemonic inputid numrandomobjs defaultformid maintableid],
+        reader: true
+      setting :empty_fields, default: %i[], reader: true
+      
+      setting :id_field, default: :departmentid, reader: true
+      setting :type_field, default: :department, reader: true
+      setting :used_in,
+        default: [
+          "ConservationReports.#{id_field}",
+          "HistEvents.#{id_field}",
+          "Loans.#{id_field}",
+          "MediaMaster.#{id_field}",
+          "Objects.#{id_field}",
+          "Projects.#{id_field}",
+          "ConXrefDetails.#{id_field}",
+          "ObjPrefixes.#{id_field}"
+        ],
+        reader: true
+      setting :mappings, default: {}, reader: true
+    end
+  end
+end
--- lib/kiba/tms/jobs/departments/prep.rb
+++ lib/kiba/tms/jobs/departments/prep.rb
@@ -5,25 +5,18 @@ module Kiba
     module Jobs
       module Departments
         module Prep
-          extend self
+          module_function
           
           def job
+            return unless config.used?
+            
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :tms__departments,
                 destination: :prep__departments
               },
-              transformer: xforms
+              transformer: config.xforms(binding)
             )
-          end
-
-          def xforms
-            Kiba.job_segment do
-              transform Tms::Transforms::DeleteTmsFields
-              transform Tms::Transforms::TmsTableNames, source: :maintableid
-              transform Delete::Fields, fields: %i[mnemonic inputid numrandomobjs defaultformid maintableid]
-              transform Tms::Transforms::DeleteNoValueTypes, field: :department
-            end
           end
         end
       end
