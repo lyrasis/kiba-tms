8c54eb29dc5b1ef830411d84be58059f638b7022
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-14 11:21:35 -0400

    make relevant methods private

--- lib/kiba/tms/jobs.rb
+++ lib/kiba/tms/jobs.rb
@@ -5,13 +5,18 @@ module Kiba
     module Jobs
       module_function
       
+      def extend_configured_jobs
+        job_namespaces.each{ |namespace| extend_configured_job(namespace) }
+      end
+
       def config_list
         @config_list ||= Kiba::Tms.constants.select do |constant|
           evaled = Kiba::Tms.const_get(constant)
           evaled.is_a?(Module) && evaled.ancestors.any?(Dry::Configurable)
         end
       end
-
+      private_class_method :config_list
+      
       def extend_configured_job(namespace)
         parent = Kiba::Tms::Jobs.const_get(namespace)
         parent.constants.each do |job|
@@ -19,18 +24,18 @@ module Kiba
           jobmod.extend(Tms::Mixins::ConfiguredJob)
         end
       end
+      private_class_method :extend_configured_job
       
-      def extend_configured_jobs
-        job_namespaces.each{ |namespace| extend_configured_job(namespace) }
-      end
-
       def has_config?(constant)
         config_list.any?(constant)
       end
-
+      private_class_method :has_config?
+      
       def job_namespaces
         Tms::Jobs.constants.select{ |constant| has_config?(constant) }
       end
+      private_class_method :job_namespaces
+      
     end
   end
 end
