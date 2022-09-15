2274f6d39d244c972b42b46348fd30886979d863
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-15 11:45:42 -0400

    add abstract ForTable job

---
+++ lib/kiba/tms/jobs/for_table.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module ForTable
+        extend Tms::Mixins::ForTable
+        module_function
+
+        def job(source:, dest:, targettable:, field: :tablename)
+          Kiba::Extend::Jobs::Job.new(
+            files: {
+              source: source,
+              destination: dest
+            },
+            transformer: for_table_xforms(table: targettable, field: field)
+          )
+        end
+      end
+    end
+  end
+end
+
