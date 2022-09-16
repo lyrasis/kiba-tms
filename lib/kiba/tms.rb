6f0952ec76c1e9a658d9232979bfe5310533a8be
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-16 11:50:12 -0400

    make prep__obj_locations configed and use TransCodes and TransStatus tables

--- lib/kiba/tms.rb
+++ lib/kiba/tms.rb
@@ -107,7 +107,9 @@ module Kiba
 
     # TMS-internal fields to be deleted
     setting :tms_fields, default: %i[loginid entereddate gsrowversion], reader: true
-    
+
+    setting :boolean_yn_mapping, default: {'0'=>'n', '1'=>'y'}, reader: true
+    setting :inverted_boolean_yn_mapping, default: {'0'=>'y', '1'=>'n'}, reader: true
     # whether conservation entity data has actually been used/augmented (true) or whether it looks like the
     #   default field data had been populated automatically by TMS (false)
     setting :conservationentity_used, default: false, reader: true
--- lib/kiba/tms/jobs/obj_locations/prep.rb
+++ lib/kiba/tms/jobs/obj_locations/prep.rb
@@ -6,64 +6,55 @@ module Kiba
       module ObjLocations
         module Prep
           module_function
-
-          # field is :inactive but I want to map in the affirmative, so logic is switched
-          INACTIVE = {
-            '0' => 'y',
-            '1' => 'n'
-          }
-
-          STATUS = {
-            '0' => 'pending',
-            '1' => 'completed',
-            '2' => 'not found',
-            '3' => 'found elsewhere',
-            '4' => 'cancelled'
-          }
-
-          TEMPFLAG = {
-            '0' => 'n',
-            '1' => 'y'
-          }
-
-          TYPE ={
-            '1' => 'temporary move',
-            '2' => 'move home',
-            '3' => 'move to new home',
-            '4' => 'inventory',
-            '5' => 'spot check',
-            '6' => 'random check',
-            '7' => 'historical entry',
-            '8' => 'scheduled temporary move',
-            '10' => 'scheduled move to new home'
-          }
          
-          def job            
+          def job
+            return unless config.used?
+            
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :tms__obj_locations,
                 destination: :prep__obj_locations,
-                lookup: %i[obj_components__with_object_numbers tms__loc_purposes]
+                lookup: lookups
               },
               transformer: xforms
             )
           end
 
+          def lookups
+            base = [:obj_components__with_object_numbers]
+            base << :prep__loc_purposes if Tms::LocPurposes.used?
+            base << :prep__trans_status if Tms::TransStatus.used?
+            base << :prep__trans_codes if Tms::TransCodes.used?
+            base
+          end
+
           def xforms
+            bind = binding
             Kiba.job_segment do
+              config = bind.receiver.send(:config)
+              
               transform Tms::Transforms::DeleteTmsFields
-              transform Delete::EmptyFields,
-                consider_blank: {
-                  dateout: '9999-12-31 00:00:00.000',
-                  loclevel: '0',
-                  tempticklerdate: '1900-01-01 00:00:00.000'
-                }
-              transform Delete::FieldValueMatchingRegexp, fields: %i[dateout], match: '^9999-12-31'
+              if config.omitting_fields?
+                transform Delete::Fields, fields: config.omitted_fields
+              end
+
+              if config.fields.any?(:dateout)
+                transform Delete::FieldValueMatchingRegexp, fields: %i[dateout], match: '^9999-12-31'
+              end
+              if config.fields.any?(:tempticklerdate)
+                transform Delete::FieldValueMatchingRegexp, fields: %i[dateout], match: '^1900'
+              end
+
               transform FilterRows::FieldEqualTo, action: :reject, field: :objlocationid, value: '-1'
-              transform Delete::FieldValueMatchingRegexp,
-                fields: %i[approver handler requestedby],
-                match: '^(\(|\[)[Nn]ot [Ee]ntered(\)|\])$'
+
+              %i[approver handler requestedby].each do |field|
+                next unless config.fields.any?(field)
+
+                transform Tms::Transforms::DeleteNoValueTypes, field: field
+              end
+              
               transform Tms::Transforms::ObjLocations::AddFulllocid
+
               transform Merge::MultiRowLookup,
                 lookup: obj_components__with_object_numbers,
                 keycolumn: :componentid,
@@ -71,19 +62,46 @@ module Kiba
                   objectnumber: :componentnumber,
                 },
                 delim: Tms.delim
-              transform Merge::MultiRowLookup,
-                lookup: tms__loc_purposes,
-                keycolumn: :locpurposeid,
-                fieldmap: {
-                  location_purpose: :locpurpose,
-                },
-                delim: Tms.delim
+
+              if Tms::LocPurposes.used?
+                transform Merge::MultiRowLookup,
+                  lookup: prep__loc_purposes,
+                  keycolumn: :locpurposeid,
+                  fieldmap: {
+                    location_purpose: :locpurpose,
+                  },
+                  delim: Tms.delim
+              end
               transform Delete::Fields, fields: %i[componentid locpurposeid]
 
-              transform Replace::FieldValueWithStaticMapping, source: :tempflag, target: :is_temp?, mapping: TEMPFLAG
-              transform Replace::FieldValueWithStaticMapping, source: :inactive, target: :active?, mapping: INACTIVE
-              transform Replace::FieldValueWithStaticMapping, source: :transstatusid, target: :status, mapping: STATUS
-              transform Replace::FieldValueWithStaticMapping, source: :transcodeid, target: :type, mapping: TYPE
+              transform Replace::FieldValueWithStaticMapping,
+                source: :tempflag,
+                target: :is_temp?,
+                mapping: Tms.boolean_yn_mapping
+              transform Replace::FieldValueWithStaticMapping,
+                source: :inactive,
+                target: :active?,
+                mapping: Tms.inverted_boolean_yn_mapping
+
+              if Tms::TransStatus.used?
+                transform Merge::MultiRowLookup,
+                  lookup: prep__trans_status,
+                  keycolumn: :transstatusid,
+                  fieldmap: {
+                    transport_status: :transstatus,
+                  },
+                  delim: Tms.delim
+              end
+              if Tms::TransCodes.used?
+                transform Merge::MultiRowLookup,
+                  lookup: prep__trans_codes,
+                  keycolumn: :transcodeid,
+                  fieldmap: {
+                    transport_type: :transcode,
+                  },
+                  delim: Tms.delim
+              end
+              transform Delete::Fields, fields: %i[transstatusid transcodeid]
             end
           end
         end
