8164b3ae26ceb51e689253335ca8836bb465c098
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-26 12:20:14 -0400

    config DimItemElemXrefs

---
+++ lib/kiba/tms/dim_item_elem_xrefs.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'dry-configurable'
+
+module Kiba
+  module Tms
+    module DimItemElemXrefs
+      extend Dry::Configurable
+      module_function
+
+      setting :delete_fields,
+        default: %i[dimitemelemxrefid displayed],
+        reader: true
+      setting :empty_fields, default: {}, reader: true
+      extend Tms::Mixins::Tableable
+
+      setting :target_tables, default: %w[], reader: true
+      extend Tms::Mixins::MultiTableMergeable
+    end
+  end
+end
--- lib/kiba/tms/jobs/dim_item_elem_xrefs/prep.rb
+++ lib/kiba/tms/jobs/dim_item_elem_xrefs/prep.rb
@@ -8,7 +8,8 @@ module Kiba
           module_function
 
           def job
-            warn("\nWARNING: Set up DimensionMethods") if Tms::DimensionMethods.used
+            return unless config.used?
+
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :tms__dim_item_elem_xrefs,
@@ -20,28 +21,44 @@ module Kiba
           end
 
           def lookups
-            base = %i[prep__dimension_elements prep__dimensions]
-            # base << :prep__dimension_methods if Tms::DimensionMethods.used
+            base = []
+            base << :prep__dimension_elements if Tms::DimensionElements.used?
+            base << :prep__dimension_methods if Tms::DimensionMethods.used?
             base
           end
-          
+
           def xforms
+            bind = binding
+
             Kiba.job_segment do
+              config = bind.receiver.send(:config)
+
               transform Tms::Transforms::DeleteTmsFields
+
+              transform FilterRows::FieldPopulated,
+                action: :keep,
+                field: :displaydimensions
+
+              if config.omitting_fields?
+                transform Delete::Fields, fields: config.omitted_fields
+                end
+
               transform Tms::Transforms::TmsTableNames
-              transform Delete::Fields, fields: %i[displayed]
               transform Rename::Field, from: :id, to: :recordid
+
+              if Tms::DimensionElements.used?
               transform Merge::MultiRowLookup,
                 lookup: prep__dimension_elements,
                 keycolumn: :elementid,
                 fieldmap: {element: :element}
+              end
               transform Delete::Fields, fields: :elementid
 
-              if Tms::DimensionMethods.used
-                # transform Merge::MultiRowLookup,
-                #   lookup: prep__dimension_methods,
-                #   keycolumn: :methodid,
-                #   fieldmap: {method: :method}
+              if Tms::DimensionMethods.used?
+                transform Merge::MultiRowLookup,
+                  lookup: prep__dimension_methods,
+                  keycolumn: :methodid,
+                  fieldmap: {method: :method}
               end
               transform Delete::Fields, fields: :methodid
             end
