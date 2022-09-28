5c29db23704583f3f3beff844e6f90d6b174ca58
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-09-27 21:26:24 -0400

    document, whitespace

--- lib/kiba/tms/mixins/configured_job.rb
+++ lib/kiba/tms/mixins/configured_job.rb
@@ -3,13 +3,24 @@
 module Kiba
   module Tms
     module Mixins
+      # {Services::ConfiguredJobExtender} automatically extends jobs
+      #   with this mixin module
+      #
+      # It provides the :config method on the job, so the job can
+      #   interact directly with the config module associated with the
+      #   job namespace.
+      #
+      # For example, all jobs under `Tms::Jobs::ObjComponents` will return
+      #   `Tms::ObjComponents` if `:config` is called.
       module ConfiguredJob
         def config
           Object.const_get(config_module_name)
         end
 
         def config_module_name
-          return class_variable_get(:@@config_module_name) if class_variable_defined?(:@@config_module_name)
+          if class_variable_defined?(:@@config_module_name)
+            return class_variable_get(:@@config_module_name)
+          end
 
           nameparts = self.name.split('::')
           nameparts.pop
@@ -17,6 +28,7 @@ module Kiba
           class_variable_set(:@@config_module_name, nameparts.join('::'))
           class_variable_get(:@@config_module_name)
         end
+        private :config_module_name
       end
     end
   end
