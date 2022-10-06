af8cdcfc567159a6eb94f4c02a349b7dccd843d0
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-10-05 21:23:30 -0400

    add RankedSourceFields transform

---
+++ lib/kiba/tms/transforms/ranked_source_fields.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Transforms
+      # Use case: you end up with multiple fields that mean the same thing coming
+      #   in from the TMS side, and the CS target is single value.
+      #
+      # Example: :accessionisodate value from ObjAccession row is different from
+      #   same field in linked RegistrationSets row. We can say we prefer the
+      #   RegistrationSets value if there is one, but if the RegistrationSets
+      #   field is blank and the ObjAccession field has a value, that value
+      #   will be used
+      class RankedSourceFields
+        # @param fields [Array<Symbol>] **ordered** - first is most highly
+        #    preferred; last is least preferred
+        # @param target [Symbol]
+        def initialize(fields:,
+                       target:,
+                       getter: Kiba::Extend::Transforms::Helpers::FieldValueGetter
+                      )
+          @fields = fields
+          @target = target
+          @deletes = fields - [target]
+          @getter = getter.new(fields: fields)
+        end
+
+        def process(row)
+          row[target] = preferred_value(row)
+          deletes.each{ |field| row.delete(field) }
+          row
+        end
+
+        private
+
+        attr_reader :fields, :target, :deletes, :getter
+
+        def preferred_value(row)
+          got = getter.call(row)
+          return nil if got.empty?
+
+          got.values
+            .first
+        end
+      end
+    end
+  end
+end
