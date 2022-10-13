f7110662ccaa2ff31312dd8d2e07c9ee14c9252a
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-10-13 10:33:25 -0400

    match norm uncontrolled names against norm pref/nonpref Constituent names in initial compile

--- lib/kiba/tms/jobs/name_compile/from_uncontrolled_name_tables.rb
+++ lib/kiba/tms/jobs/name_compile/from_uncontrolled_name_tables.rb
@@ -9,11 +9,13 @@ module Kiba
 
           def job
             return unless Tms::NameCompile.used?
+            return if target_tables.empty?
 
             Kiba::Extend::Jobs::MultiSourcePrepJob.new(
               files: {
                 source: sources,
-                destination: :name_compile__from_uncontrolled_name_tables
+                destination: :name_compile__from_uncontrolled_name_tables,
+                lookup: lookups
               },
               transformer: xforms,
               helper: Kiba::Tms::NameCompile.multi_source_normalizer
@@ -21,21 +23,79 @@ module Kiba
           end
 
           def sources
-            Tms::NameCompile.uncontrolled_name_source_tables
-              .keys
+            target_tables
               .map{ |key| Tms.const_get(key) }
               .map{ |mod| "name_compile_from__#{mod.filekey}".to_sym }
           end
 
+          def lookups
+            base = %i[
+                      constituents__by_nonpref_norm
+                      constituents__by_norm
+                     ]
+            if ntc_needed?
+              base << :name_type_cleanup__for_uncontrolled_name_tables
+            end
+            base
+          end
+
+          def target_tables
+            config.uncontrolled_name_source_tables
+              .keys
+              .select{ |key| ntc_targets.any?(key) }
+          end
+
+          def ntc_needed?
+            return false unless ntc_done?
+
+            !target_tables.empty?
+          end
+          extend Tms::Mixins::NameTypeCleanupable
+
           def xforms
+            bind = binding
+
             Kiba.job_segment do
+              cleanable = bind.receiver.send(:ntc_needed?)
+              prefname = Tms::Constituents.preferred_name_field
+
               transform Tms::Transforms::NameTypeCleanup::ExtractIdSegment,
-                target: :name,
+                target: :origname,
                 segment: :name
               transform Kiba::Extend::Transforms::Cspace::NormalizeForID,
-                source: :name,
+                source: :origname,
                 target: :constituentid
               transform Deduplicate::Table, field: :constituentid
+
+              [constituents__by_nonpref_norm,
+               constituents__by_norm].each do |lkup|
+                transform Merge::MultiRowLookup,
+                  lookup: lkup,
+                  keycolumn: :constituentid,
+                  fieldmap: {
+                    concontype: :contype,
+                    conprefname: prefname
+                  }
+                transform do |row|
+                  contype = row[:contype]
+                  next row unless contype.blank?
+
+                  conpref = row[:conprefname]
+                  next row if conpref.blank?
+
+                  row[prefname] = row[:conprefname]
+                  row[:contype] = row[:concontype]
+                  row
+                end
+                transform Delete::Fields, fields: %i[concontype conprefname]
+              end
+
+
+
+              if cleanable
+                # transform Tms::Transforms::NameTypeCleanup::OverlayAll,
+                #   lookup: name_type_cleanup__for_uncontrolled_name_tables
+              end
             end
           end
         end
