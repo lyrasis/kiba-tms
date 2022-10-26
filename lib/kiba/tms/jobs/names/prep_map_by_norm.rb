af77c54efb35ffc961d9ef5d58fb52f03a58a61d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-10-26 11:25:44 -0400

    limit to persons and orgs from Constituents, and uncontrolled

--- lib/kiba/tms/jobs/names/prep_map_by_norm.rb
+++ lib/kiba/tms/jobs/names/prep_map_by_norm.rb
@@ -17,17 +17,6 @@ module Kiba
             )
           end
 
-          # def sources
-          #   if Tms::Names.cleanup_iteration && Tms::Names.cleanup_workflow == :old
-          #     iter = Tms::Names.cleanup_iteration
-          #     "nameclean#{iter}__prep".to_sym
-          #   elsif Tms::Names.cleanup_iteration && !Tms::Names.cleanup_workflow == :old
-          #     warn("#{self.name}: Need to implement new workflow when cleanup iteration")
-          #   else
-          #     :name_compile__unique
-          #   end
-          # end
-
           def xforms
             Kiba.job_segment do
               transform FilterRows::WithLambda,
@@ -35,7 +24,15 @@ module Kiba
                 lambda: ->(row) do
                   rt = row[:relation_type]
                   norm = row[:prefnormorig]
-                  rt && rt == '_main term' && !norm.blank?
+                  ts = row[:termsource]
+                  sources = [
+                    'TMS Constituents.orgs',
+                    'TMS Constituents.persons',
+                    'Uncontrolled'
+                  ]
+                  rt && rt == '_main term' &&
+                    ts && sources.any?(ts) &&
+                    !norm.blank?
                 end
               transform Delete::FieldsExcept,
                 fields: %i[contype name prefnormorig]
@@ -47,36 +44,6 @@ module Kiba
               transform Tms::Transforms::Constituents::AddDefaultContype
             end
           end
-
-          # def old_cleanup_xforms
-          #   Kiba.job_segment do
-          #     prefname = Tms::Constituents.preferred_name_field
-          #     keepfields = %i[constituenttype fp_norm norm] + [prefname]
-          #     transform Delete::FieldsExcept, fields: keepfields
-          #     transform Append::NilFields, fields: %i[person organization]
-          #     transform do |row|
-          #       case row[:constituenttype]
-          #       when 'Person'
-          #         row[:person] = row[prefname]
-          #       when 'Organization'
-          #         row[:organization] = row[prefname]
-          #       else
-          #         row[Tms::Constituents.untyped_default.downcase.to_sym] = row[prefname]
-          #       end
-          #       row
-          #     end
-          #     transform Rename::Field, from: :fp_norm, to: :orig_norm
-          #     transform do |row|
-          #       orignorm = row[:orig_norm]
-          #       if orignorm.blank?
-          #         row[:orig_norm] = row[:norm]
-          #       end
-          #       row
-          #     end
-          #     transform Delete::Fields, fields: ( %i[constituenttype norm] + [prefname] )
-          #     transform Deduplicate::Table, field: :orig_norm
-          #   end
-          # end
         end
       end
     end
