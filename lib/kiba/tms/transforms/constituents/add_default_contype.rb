bf044905a6c16dd84107c3dcce5f648a0f6169dd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-10-25 20:13:26 -0400

    add Constituents::AddDefaultContype transform

---
+++ lib/kiba/tms/transforms/constituents/add_default_contype.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Transforms
+      module Constituents
+        class AddDefaultContype
+          def initialize(target: :contype)
+            @target = target
+            @default = Tms::Constituents.untyped_default
+          end
+
+          # @private
+          def process(row)
+            type = row[target]
+            return row unless type.blank?
+
+            row[target] = default
+            row
+          end
+
+          private
+
+          attr_reader :target, :default
+        end
+      end
+    end
+  end
+end
