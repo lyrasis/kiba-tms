01908d6874b3cd3309d5d70535807879131aab0c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-11-08 16:08:48 -0500

    add config options

--- lib/kiba/tms/locations.rb
+++ lib/kiba/tms/locations.rb
@@ -10,11 +10,11 @@ module Kiba
 
       extend Tms::Mixins::Tableable
 
+      # which authority types to process records and hierarchies for
+      #   (organizations used as locations are handled a bit separately
+      setting :authorities, default: %i[local offsite], reader: true
+      setting :brief_address_mappings, default: {}, reader: true
       setting :cleanup_iteration, default: 0, reader: true
-      # Whether client wants the migration to include construction of a location
-      #   hierarchy
-      setting :hierarchy, default: true, reader: true
-      setting :hierarchy_delim, default: ' > ', reader: true
       # Which fields in obj_locations need to be concatenated with the location
       #   value to create additional location values (and thus need a unique id
       #   added to look them up)
@@ -22,9 +22,15 @@ module Kiba
         default: %i[locationid loclevel searchcontainer temptext shipmentid
                     crateid sublevel],
         reader: true
-      # which authority types to process records and hierarchies for
-      #   (organizations used as locations are handled a bit separately
-      setting :authorities, default: %i[local offsite], reader: true
+      # Whether client wants the migration to include construction of a location
+      #   hierarchy
+      setting :hierarchy, default: true, reader: true
+      setting :hierarchy_delim, default: ' > ', reader: true
+      setting :initial_data_cleaner, default: nil, reader: true
+      # base fields from Locations table to concatenate into location name
+      setting :loc_fields,
+        default: %i[brief_address site room unittype unitnumber unitposition],
+        reader: true
       setting :multi_source_normalizer,
         default: Kiba::Extend::Utils::MultiSourceNormalizer.new,
         reader: true
