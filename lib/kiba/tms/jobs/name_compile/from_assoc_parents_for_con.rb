0c42e587727fd32b4392a12d49f50eb998141c03
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-02 20:17:19 -0500

    Remove names that match any norm Constituent form

--- lib/kiba/tms/jobs/name_compile/from_assoc_parents_for_con.rb
+++ lib/kiba/tms/jobs/name_compile/from_assoc_parents_for_con.rb
@@ -14,7 +14,8 @@ module Kiba
             Kiba::Extend::Jobs::MultiSourcePrepJob.new(
               files: {
                 source: :assoc_parents_for__constituents,
-                destination: :name_compile__from_assoc_parents_for_con
+                destination: :name_compile__from_assoc_parents_for_con,
+                lookup: :constituents__by_all_norms
               },
               transformer: xforms,
               helper: Kiba::Tms::NameCompile.multi_source_normalizer
@@ -26,6 +27,17 @@ module Kiba
               transform Tms::Transforms::NameCompile::ExtractNamesFromTable,
                 table: 'AssocParents.for_constituents',
                 fields: [:childstring]
+              transform Kiba::Extend::Transforms::Cspace::NormalizeForID,
+                source: Tms::Constituents.preferred_name_field,
+                target: :norm
+              transform Merge::MultiRowLookup,
+                lookup: constituents__by_all_norms,
+                keycolumn: :norm,
+                fieldmap: {contype: :contype},
+                conditions: ->(_r, rows){ [rows.first] }
+              transform FilterRows::FieldPopulated,
+                action: :reject,
+                field: :contype
             end
           end
         end
