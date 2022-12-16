2d473170c856569c4350aebfa6dc6a353f3140a7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-12-15 20:22:37 -0500

    Add Prep::ObjInsurance job

---
+++ lib/kiba/tms/jobs/obj_insurance/prep.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module ObjInsurance
+        module Prep
+          module_function
+
+          def job
+            return unless config.used?
+
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :tms__obj_insurance,
+                destination: :prep__obj_insurance,
+                lookup: lookups
+              },
+              transformer: xforms
+            )
+          end
+
+          def lookups
+            base = %i[
+                      names__by_altnameid
+                      objects__number_lookup
+                     ]
+            base << :prep__valuation_purposes if purposes?
+            base << :prep__currencies if currencies?
+            base
+          end
+
+          def currencies?
+            Tms::Currencies.used &&
+              Tms.job_output?(:prep__currencies)
+          end
+
+          def purposes?
+            Tms::ValuationPurposes.used &&
+              Tms.job_output?(:prep__valuation_purposes)
+          end
+
+          def xforms
+            bind = binding
+
+            Kiba.job_segment do
+              config = bind.receiver.send(:config)
+              prefname = Tms::Constituents.preferred_name_field
+
+              transform Tms::Transforms::DeleteTmsFields
+              if config.omitting_fields?
+                transform Delete::Fields, fields: config.omitted_fields
+              end
+
+              if config.zero_value_treatment == :drop
+                transform FilterRows::FieldEqualTo,
+                  action: :reject,
+                  field: :value,
+                  value: '.0000'
+              end
+
+              transform Merge::MultiRowLookup,
+                lookup: objects__number_lookup,
+                keycolumn: :objectid,
+                fieldmap: {objectnumber: :objectnumber}
+              transform Delete::Fields, fields: :objectid
+
+              transform Merge::MultiRowLookup,
+                lookup: names__by_altnameid,
+                keycolumn: :appraiserid,
+                fieldmap: {valuesourcepersonlocal: :person},
+                conditions: ->(_r, rows) do
+                  rows.reject{ |row| row[:person].blank? }
+                end
+              transform Merge::MultiRowLookup,
+                lookup: names__by_altnameid,
+                keycolumn: :appraiserid,
+                fieldmap: {valuesourceorganizationlocal: :org},
+                conditions: ->(_r, rows) do
+                  rows.reject{ |row| row[:org].blank? }
+                end
+              transform Delete::Fields, fields: :appraiserid
+
+              if bind.receiver.send(:purposes?)
+                transform Merge::MultiRowLookup,
+                  lookup: prep__valuation_purposes,
+                  keycolumn: Tms::ValuationPurposes.id_field,
+                  fieldmap: {
+                    Tms::ValuationPurposes.type_field =>
+                      Tms::ValuationPurposes.type_field
+                  }
+              end
+              transform Delete::Fields, fields: :valuationpurposeid
+
+              if bind.receiver.send(:currencies?)
+                transform Merge::MultiRowLookup,
+                  lookup: prep__currencies,
+                  keycolumn: config.pref_currencyid,
+                  fieldmap: {
+                    Tms::Currencies.type_field =>
+                      Tms::Currencies.type_field
+                  }
+              end
+              transform Delete::Fields, fields: config.pref_currencyid
+
+              transform Replace::FieldValueWithStaticMapping,
+                source: :systemvaluetype,
+                mapping: config.systemvaluetype_mapping
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/kiba/tms/obj_insurance.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'dry-configurable'
+
+module Kiba
+  module Tms
+    module ObjInsurance
+      extend Dry::Configurable
+      module_function
+
+      setting :checkable, default: {
+        adjval_eq_val: Proc.new do
+          Tms::Services::CompareFieldPairValuesChecker.call(
+            mod: self,
+            fields: %i[value adjustedvalue]
+          )
+        end,
+        currval_eq_val: Proc.new do
+          Tms::Services::CompareFieldPairValuesChecker.call(
+            mod: self,
+            fields: %i[value currencyvalue]
+          )
+        end
+      },
+        reader: true
+
+      # :currencyid or :localcurrencyid
+      # So far, localcurrencyid has data where currencyid does not, so we prefer
+      #   it
+      setting :pref_currencyid,
+        default: :localcurrencyid,
+        reader: true
+      setting :nonpref_currencyid,
+        reader: true,
+        constructor: ->(value){
+          if pref_currencyid == :currencyid
+            :localcurrencyid
+          else
+            :currencyid
+          end
+        }
+      setting :systemvaluetype_mapping,
+        default: {
+          '0'=>'object insurance value',
+          '1'=>'loan object insurance value',
+          '2'=>'loan object 3rd party appraisal value',
+          '3'=>'shipment insurance value',
+          '4'=>'object accession value'
+        },
+        reader: true
+      # What to do with row if :value = 0
+      # :drop or :create_record
+      setting :zero_value_treatment,
+        default: :drop,
+        reader: true
+      setting :delete_fields,
+        default: %i[currencyrate2 currencyrateisodate currencyvalue
+                    iscurrent isratelocked
+                    riskfactor riskfactorisodate
+                    roundedisodate roundedvalue roundeddecimals
+                    adjustedvalue],
+        reader: true,
+        constructor: ->(value){ value << nonpref_currencyid }
+      extend Tms::Mixins::Tableable
+    end
+  end
+end
