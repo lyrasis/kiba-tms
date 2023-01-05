21fc7a780076cf82eb629963e6ff3afee8ac23f5
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-01-04 19:06:47 -0500

    Add param controlling whether to add suffix if only one row with base number

--- lib/kiba/tms/transforms/id_generator.rb
+++ lib/kiba/tms/transforms/id_generator.rb
@@ -5,15 +5,19 @@ module Kiba
     module Transforms
       class IdGenerator
         include Tms::Mixins::DateSortable
-
+        # @param omit_suffix_if_single [Boolean] if true and there is only one
+        #   row with a given id source value, no suffix is added to id_target
+        #   value
         def initialize(prefix: '', id_source:, id_target:, sort_on: nil,
-                       separator: '.', delete_source: true)
+                       separator: '.', delete_source: true,
+                       omit_suffix_if_single: true)
           @prefix = prefix
           @id_source = id_source
           @id_target = id_target
           @sort_on = sort_on
           @separator = separator
           @delete_source = delete_source
+          @omit_suffix_if_single = omit_suffix_if_single
           @data = {}
         end
 
@@ -28,23 +32,39 @@ module Kiba
         def close
           @data.values
             .map{ |rows| generate_ids(rows) }
+            .map{ |rows| delete_sources(rows) }
             .each{ |rows| rows.each{ |row| yield row } }
         end
 
         private
 
         attr_reader :prefix, :id_source, :id_target, :sort_on,
-          :separator, :delete_source
+          :separator, :delete_source, :omit_suffix_if_single
+
+        def delete_sources(rows)
+          return rows unless delete_source
+          return rows unless source_deleteable?
+
+          rows.map{ |row| row.delete(id_source); row }
+        end
 
         def generate_id(row, idx)
           num = idx + 1
           itemid = row[id_source]
-          row[id_target] = "#{prefix}#{itemid}#{separator}#{num.to_s.rjust(3, '0')}"
-          row.delete(id_source) if delete_source && source_deleteable?
+          id = "#{prefix}#{itemid}#{separator}#{num.to_s.rjust(3, '0')}"
+          row[id_target] = id
           row
         end
 
         def generate_ids(rows)
+          if rows.length == 1 && omit_suffix_if_single
+            rows.map{ |row| row[id_target] = row[id_source]; row }
+          else
+            generate_multirow_ids(rows)
+          end
+        end
+
+        def generate_multirow_ids(rows)
           if sort_on
             rows.sort_by{ |row| sortable_date_from_row(row, sort_on) }
               .map.with_index{ |row, idx| generate_id(row, idx) }
