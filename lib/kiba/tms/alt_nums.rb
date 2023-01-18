26a7af6a4773f517899497de8c2572bc63385a5c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-01-18 10:39:06 -0500

    Set up config for merging in cleaned up Objects AltNums types

--- lib/kiba/tms/alt_nums.rb
+++ lib/kiba/tms/alt_nums.rb
@@ -16,9 +16,33 @@ module Kiba
       setting :target_table_type_cleanup_needed,
         default: [],
         reader: true
+      # Names of completed CSV files for any cleaned up (by client) target
+      #   table worksheets. Expected to be found in `base_dir/supplied`
+      #   directory
+      # Format like:
+      #   {
+      #     'Objects'=>[
+      #        'alt_num_types_for_objects_2023-01-18.csv',
+      #        'alt_num_types_for_objects_2022-12-21.csv',
+      #     ]
+      #   }
+      # Worksheets for a given target table are in newest-to-oldest order
       setting :target_table_type_cleanup_done,
-        default: [],
+        default: {},
         reader: true
+
+      def cleaned_files_for(tablename)
+        return [] unless target_table_type_cleanup_done.key?(tablename)
+
+        target_table_type_cleanup_done[tablename]
+      end
+
+      def cleanup_done_for?(tablename)
+        return false unless target_table_type_cleanup_done.key?(tablename)
+        return false if target_table_type_cleanup_done[tablename].blank?
+
+        true
+      end
     end
   end
 end
