38ddb7737f8d7747f99da5f68c1f6d74415106a2
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-01-30 19:07:22 -0500

    Remove rows where cleaned prefname equals institution value

--- lib/kiba/tms/jobs/name_compile/from_con_org_with_inst.rb
+++ lib/kiba/tms/jobs/name_compile/from_con_org_with_inst.rb
@@ -11,32 +11,62 @@ module Kiba
             Kiba::Extend::Jobs::MultiSourcePrepJob.new(
               files: {
                 source: :constituents__for_compile,
-                destination: :name_compile__from_con_org_with_inst
+                destination: jobkey,
+                lookup: lookups
               },
               transformer: xforms,
-              helper: Kiba::Tms::NameCompile::multi_source_normalizer
+              helper: config.multi_source_normalizer
             )
           end
 
+          def jobkey
+            :name_compile__from_con_org_with_inst
+          end
+
+          def lookups
+            base = []
+            base << :constituents__by_all_norms if Tms::NameTypeCleanup.done
+            base.select{ |job| Tms.job_output?(job) }
+          end
+
           def xforms
+            bind = binding
+
             Kiba.job_segment do
-              treatment = Tms::NameCompile.source_treatment[:name_compile__from_con_org_with_inst]
+              job = bind.receiver
+              config = job.send(:config)
+              treatment = config.source_treatment[job.send(:jobkey)]
+              prefname = Tms::Constituents.preferred_name_field
+
               transform FilterRows::WithLambda,
                 action: :keep,
                 lambda: ->(row) do
                   type = row[:contype]
                   type && type.start_with?('Organization')
                 end
-              transform FilterRows::FieldPopulated, action: :keep, field: :institution
-              transform Append::ToFieldValue, field: :constituentid, value: '.institution'
-              transform Merge::ConstantValue, target: :termsource, value: 'TMS Constituents.orgs_institution'
+              transform FilterRows::FieldPopulated,
+                action: :keep,
+                field: :institution
+              transform Append::ToFieldValue,
+                field: :constituentid,
+                value: '.institution'
+              transform Merge::ConstantValue,
+                target: :termsource,
+                value: 'TMS Constituents.orgs_institution'
+
+              if Tms::NameTypeCleanup.done
+                transform FilterRows::WithLambda,
+                  action: :reject,
+                  lambda: ->(row){ row[prefname] == row[:institution] }
+              end
 
               if treatment == :variant
-                transform Merge::ConstantValue, target: :relation_type, value: 'variant term'
-                
+                transform Merge::ConstantValue,
+                  target: :relation_type,
+                  value: 'variant term'
                 transform Rename::Field, from: :institution, to: :variant_term
               end
-              
+
               transform Delete::Fields, fields: Tms::NameCompile.org_nil
             end
           end
