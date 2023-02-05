7b10b4fb529371a65771580e5224a77c000c74eb
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-05 14:11:20 -0500

    Add configurable sort type

--- lib/kiba/tms/transforms/id_generator.rb
+++ lib/kiba/tms/transforms/id_generator.rb
@@ -9,15 +9,17 @@ module Kiba
         #   row with a given id source value, no suffix is added to id_target
         #   value
         def initialize(prefix: '', id_source:, id_target:, sort_on: nil,
-                       separator: '.', delete_source: true,
-                       omit_suffix_if_single: true)
+                       sort_type: :date, separator: '.', delete_source: true,
+                       omit_suffix_if_single: true, padding: 3)
           @prefix = prefix
           @id_source = id_source
           @id_target = id_target
           @sort_on = sort_on
+          @sort_type = sort_type
           @separator = separator
           @delete_source = delete_source
           @omit_suffix_if_single = omit_suffix_if_single
+          @padding = padding
           @data = {}
         end
 
@@ -38,8 +40,8 @@ module Kiba
 
         private
 
-        attr_reader :prefix, :id_source, :id_target, :sort_on,
-          :separator, :delete_source, :omit_suffix_if_single
+        attr_reader :prefix, :id_source, :id_target, :sort_on, :sort_type,
+          :separator, :delete_source, :omit_suffix_if_single, :padding
 
         def delete_sources(rows)
           return rows unless delete_source
@@ -51,7 +53,7 @@ module Kiba
         def generate_id(row, idx)
           num = idx + 1
           itemid = row[id_source]
-          id = "#{prefix}#{itemid}#{separator}#{num.to_s.rjust(3, '0')}"
+          id = "#{prefix}#{itemid}#{separator}#{num.to_s.rjust(padding, '0')}"
           row[id_target] = id
           row
         end
@@ -59,6 +61,8 @@ module Kiba
         def generate_ids(rows)
           if rows.length == 1 && omit_suffix_if_single
             rows.map{ |row| row[id_target] = "#{prefix}#{row[id_source]}"; row }
+          elsif rows.length == 1
+            [generate_id(rows[0], 0)]
           else
             generate_multirow_ids(rows)
           end
@@ -66,10 +70,26 @@ module Kiba
 
         def generate_multirow_ids(rows)
           if sort_on
-            rows.sort_by{ |row| sortable_date_from_row(row, sort_on) }
-              .map.with_index{ |row, idx| generate_id(row, idx) }
+            sorted_rows(rows).map.with_index{ |row, idx| generate_id(row, idx) }
           else
             rows.map.with_index{ |row, idx| generate_id(row, idx) }
+          end
+        end
+
+        def sorted_rows(rows)
+          case sort_type
+          when :date
+            rows.sort_by{ |row| sortable_date_from_row(row, sort_on) }
+          when :i
+            rows.sort_by do |row|
+              val = row[sort_on]
+              val.blank? ? 0 : val.to_i
+            end
+          else
+            rows.sort_by do |row|
+              val = row[sort_on]
+              val.blank? ? '' : val
+            end
           end
         end
 
