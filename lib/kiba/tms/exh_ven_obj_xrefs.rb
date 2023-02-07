a7e78d11d5b663b6da1d2114dac262c6d25c98a1
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-06 19:41:36 -0500

    Add documentation

--- lib/kiba/tms/exh_ven_obj_xrefs.rb
+++ lib/kiba/tms/exh_ven_obj_xrefs.rb
@@ -8,10 +8,19 @@ module Kiba
       extend Dry::Configurable
       module_function
 
-      setting :delete_fields,
-        default: %i[lightexpluxperhour remarks begindispldateiso enddispldateiso
-                    catalognumber],
-        reader: true
+      # ## ABOUT THIS TABLE
+      #
+      # Contains specific relationship data that is not modeled in a structured
+      #   way (or, really, at all) in CS. If this table contains data a client
+      #   needs to migrate, the structured data in the table needs to be
+      #   collapsed down into a free-text note value. Then, that note needs to
+      #   be merged into either Object record (with a label indicating which
+      #   exhibition and venue the note describes) or Exhibition record (with a
+      #   label indicating which object and venue the note describes)
+      #
+      # For mmm, no new Exhibition/Object relationships could be derived from
+      #   this table (in addition to those already derived from ExhObjXrefs.
+
       extend Tms::Mixins::Tableable
     end
   end
