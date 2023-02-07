10a28e8894ef5f568b94e8d32c8dee79e8951dc7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-07 17:16:25 -0500

    Safely return if not used

--- lib/kiba/tms/jobs/disposition_methods/prep.rb
+++ lib/kiba/tms/jobs/disposition_methods/prep.rb
@@ -6,8 +6,10 @@ module Kiba
       module DispositionMethods
         module Prep
           module_function
-          
+
           def job
+            return unless config.used
+
             Kiba::Extend::Jobs::Job.new(
               files: {
                 source: :tms__disposition_methods,
