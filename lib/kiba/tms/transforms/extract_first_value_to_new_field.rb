16ca4cb740955821a06e47d35d688be152ea774f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-08 16:26:13 -0500

    Add ExtractFirstValueToNewField xform

---
+++ lib/kiba/tms/transforms/extract_first_value_to_new_field.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Transforms
+      class ExtractFirstValueToNewField
+        def initialize(source:, newfield:, delim: Tms.delim)
+          @source = source
+          @newfield = newfield
+          @delim = delim
+        end
+
+        def process(row)
+          row[newfield] = nil
+          val = row[source]
+          return row if val.blank?
+
+          vals = val.split(delim)
+          if vals.length == 1
+            row[newfield] = val
+            row[source] = nil
+          else
+            row[newfield] = vals.shift
+            row[source] = vals.join(delim)
+          end
+          row
+        end
+
+        private
+
+        attr_reader :source, :newfield, :delim
+
+      end
+    end
+  end
+end
