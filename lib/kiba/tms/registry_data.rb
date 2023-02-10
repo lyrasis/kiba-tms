c389830d924ce62bc44fc9b91284528b0305e996
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-02-10 18:03:55 -0500

    Delete superseded job

--- lib/kiba/tms/registry_data.rb
+++ lib/kiba/tms/registry_data.rb
@@ -2878,15 +2878,6 @@ module Kiba
           }
         end
 
-        Kiba::Tms.registry.namespace('status_flags') do
-          register :new_tables, {
-            creator: Kiba::Tms::Jobs::StatusFlags::NewTables,
-            path: File.join(Kiba::Tms.datadir, 'reports', 'status_flags_new_tables.csv'),
-            desc: 'Status flags for merge into tables we do not have set up yet. Non-zero count means work to do!',
-            tags: %i[status_flags todochk]
-          }
-        end
-
         Kiba::Tms.registry.namespace('terms') do
           register :used_in_xrefs, {
             creator: Kiba::Tms::Jobs::Terms::UsedInXrefs,
