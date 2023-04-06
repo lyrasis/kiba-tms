71bd799540a41b5266691c09966639133c6908bf
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-04-06 15:24:32 -0400

    Set up ObjDates table handling

---
+++ lib/kiba/tms/jobs/obj_dates/prep.rb
@@ -0,0 +1,79 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module Jobs
+      module ObjDates
+        module Prep
+          module_function
+
+          def job
+            return unless config.used?
+
+            Kiba::Extend::Jobs::Job.new(
+              files: {
+                source: :tms__obj_dates,
+                destination: :prep__obj_dates,
+                lookup: lookups
+              },
+              transformer: xforms
+            )
+          end
+
+          def lookups
+            base = %i[
+                      objects__numbers_cleaned
+                     ]
+            base
+          end
+
+          def xforms
+            bind =  binding
+
+            Kiba.job_segment do
+              config = bind.receiver.send(:config)
+
+              if config.drop_inactive
+                transform FilterRows::FieldEqualTo,
+                  action: :keep,
+                  field: :active,
+                  value: '1'
+              end
+              transform Delete::Fields, fields: :active
+
+              transform Tms::Transforms::DeleteTmsFields
+
+              if config.omitting_fields?
+                transform Delete::Fields, fields: config.omitted_fields
+              end
+              transform Tms.data_cleaner if Tms.data_cleaner
+
+              transform Clean::RegexpFindReplaceFieldVals,
+                fields: :eventtype,
+                find: '^[(\[]not entered[)\]]$',
+                replace: '',
+                casesensitive: false
+
+              transform Clean::RegexpFindReplaceFieldVals,
+                fields: %i[datebegsearch dateendsearch daybegsearch dayendsearch
+                           monthbegsearch monthendsearch dateeffectiveisodate],
+                find: '^0$',
+                replace: ''
+
+              transform FilterRows::AnyFieldsPopulated,
+                action: :keep,
+                fields: config.content_fields
+              transform Merge::MultiRowLookup,
+                lookup: objects__numbers_cleaned,
+                keycolumn: :objectid,
+                fieldmap: {objectnumber: :objectnumber}
+              transform FilterRows::FieldPopulated,
+                action: :keep,
+                field: :objectnumber
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/kiba/tms/obj_dates.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Kiba
+  module Tms
+    module ObjDates
+      extend Dry::Configurable
+      module_function
+
+      setting :delete_fields,
+        default: %i[],
+        reader: true
+      setting :empty_fields,
+        default: {
+          :eventtype=>[nil, "", "(not entered)", "[not entered]"]
+        },
+        reader: true
+      setting :non_content_fields,
+        default: %i[objdateid objectid],
+        reader: true
+      extend Tms::Mixins::Tableable
+
+      # Whether to remove rows marked inactive from migration. If true, only
+      #   rows with :active == 1 will be kept.
+      setting :drop_inactive, default: false, reader: true
+
+      # # Project-specific transforms to prepare ConGeography data for merge
+      # #   into person and org records.
+      # #
+      # # To be compatible with the default mergers, the cleaner should add the
+      # #   following fields to each row:
+      # #
+      # # - :type - allowed values: birth, death, blank
+      # # - :mergeable - the value (with any necessary prefix) to merge
+      # setting :cleaner,
+      #   default: nil,
+      #   reader: true
+
+      # setting :merger,
+      #   default: Tms::Transforms::ConGeography::PersonMerger,
+      #   reader: true
+    end
+  end
+end
