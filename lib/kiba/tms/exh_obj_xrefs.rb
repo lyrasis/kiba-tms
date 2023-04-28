08d0d19e4ada6ec29c5cc22f9b3c5909d071ed59
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-04-28 13:17:32 -0400

    Delete unneeded requires

--- lib/kiba/tms/exh_obj_xrefs.rb
+++ lib/kiba/tms/exh_obj_xrefs.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'dry-configurable'
-
 module Kiba
   module Tms
     module ExhObjXrefs
@@ -12,6 +10,12 @@ module Kiba
         default: %i[],
         reader: true
       extend Tms::Mixins::Tableable
+
+      # Options: :exhibited_object_information, :drop, :exhibition_planning_note
+      # Others may be developed
+      setting :text_entry_handling,
+        default: :exhibited_object_information,
+        reader: true
     end
   end
 end
