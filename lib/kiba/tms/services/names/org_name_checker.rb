3dc3485ce5e29dd1452e2804fcb2838dbfc9309a
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-05-16 18:24:32 -0400

    Move org name check logic to kiba-extend helper and use that here

--- lib/kiba/tms/services/names/org_name_checker.rb
+++ lib/kiba/tms/services/names/org_name_checker.rb
@@ -4,47 +4,26 @@ module Kiba
   module Tms
     module Services
       module Names
-        # Indicates whether value of field contains terms indicating organization-ness
+        # Indicates whether value of field contains terms indicating
+        #   organization-ness
         class OrgNameChecker
           def initialize(field:)
             @field = field
+            @checker = Kiba::Extend::Transforms::Helpers::OrgNameChecker.new(
+              family_is_org: true
+            )
           end
 
           def call(row)
             val = row[field]
             return false if val.blank?
 
-            true if patterns.any? { |pattern| val.downcase.match?(pattern) }
+            checker.call(val)
           end
 
           private
 
-          attr_reader :field
-
-          def patterns
-            [
-              " LLC",
-              " co$",
-              ' co\.',
-              " dept$",
-              " inc$",
-              '^\w+ & \w+$',
-              "college",
-              "company",
-              "department",
-              'dept\.',
-              "foundation",
-              "gallery",
-              'inc\.',
-              "library",
-              "museum",
-              "observatory",
-              "publish",
-              "service",
-              "studio",
-              "university"
-            ].map { |pattern| Regexp.new(pattern) }
-          end
+          attr_reader :checker, :field
         end
       end
     end
