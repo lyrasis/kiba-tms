fc06e05e73c97875a1acae0693ed6d84bec973ed
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-09-13 17:32:29 -0400

    noblame: Whitespace and/or formatting changes only

--- lib/kiba/tms/alt_nums_for_obj_type_cleanup.rb
+++ lib/kiba/tms/alt_nums_for_obj_type_cleanup.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
-module Kiba::Tms::AltNumsForObjTypeCleanup
-  module_function
+module Kiba
+  module Tms
+    module AltNumsForObjTypeCleanup
+      module_function
 
-  extend Dry::Configurable
+      extend Dry::Configurable
 
-  setting :base_job,
-    default: :alt_nums__types_for_objects,
-    reader: true
+      setting :base_job,
+        default: :alt_nums__types_for_objects,
+        reader: true
 
-  setting :fingerprint_fields,
-    default: %i[number_type correct_type treatment note],
-    reader: true
+      setting :fingerprint_fields,
+        default: %i[number_type correct_type treatment note],
+        reader: true
 
-  extend Kiba::Extend::Mixins::IterativeCleanup
+      extend Kiba::Extend::Mixins::IterativeCleanup
 
-  def job_tags
-    %i[alt_nums objects cleanup]
-  end
+      def job_tags
+        %i[alt_nums objects cleanup]
+      end
 
-  def worksheet_add_fields
-    %i[correct_type treatment note]
-  end
+      def worksheet_add_fields
+        %i[correct_type treatment note]
+      end
 
   def worksheet_field_order
     %i[number_type correct_type treatment note
@@ -29,8 +31,9 @@ module Kiba::Tms::AltNumsForObjTypeCleanup
       occs_with_enddate
       example_rec_nums example_values]
   end
+      def fingerprint_flag_ignore_fields
+        %i[number_type]
+      end
 
-  def fingerprint_flag_ignore_fields
-    %i[number_type]
   end
 end
