234655d70e288dc801ebb5089dbd190f3215c8fe
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-09-13 19:20:30 -0400

    Extract shared cleanup settings to module

--- lib/kiba/tms/alt_nums_for_obj_type_cleanup.rb
+++ lib/kiba/tms/alt_nums_for_obj_type_cleanup.rb
@@ -12,77 +12,14 @@ module Kiba
         reader: true
 
       setting :fingerprint_fields,
-        default: %i[number_type correct_type treatment note],
+        default: Tms::AltNumsCleanupShared.clean_fingerprint_fields,
         reader: true
 
       extend Kiba::Extend::Mixins::IterativeCleanup
+      extend Tms::AltNumsCleanupShared
 
       def job_tags
-        %i[alt_nums objects cleanup]
-      end
-
-      def worksheet_add_fields
-        %i[correct_type treatment note]
-      end
-
-      def worksheet_field_order
-        [:number_type, :correct_type, :treatment, :note,
-          collate_fields].flatten
-      end
-
-      def occ_fields
-        %i[occurrences occs_with_remarks occs_with_begindate
-          occs_with_enddate]
-      end
-
-      def collate_fields
-        [occ_fields, :example_rec_nums, :example_values]
-      end
-
-      def fingerprint_flag_ignore_fields
-        %i[number_type]
-      end
-
-      setting :orig_fingerprint_fields,
-        default: %i[number_type],
-        reader: true
-
-      def base_job_cleaned_pre_xforms
-        bind = binding
-
-        Kiba.job_segment do
-          mod = bind.receiver
-
-          transform Fingerprint::Add,
-            target: :fingerprint,
-            fields: mod.orig_fingerprint_fields
-        end
-      end
-
-      def cleaned_uniq_post_xforms
-        bind = binding
-
-        Kiba.job_segment do
-          mod = bind.receiver
-
-          mod.occ_fields.each do |field|
-            transform Kiba::Tms::Transforms::SumCollatedOccurrences,
-              field: field,
-              delim: mod.collation_delim
-          end
-        end
-      end
-
-      def final_post_xforms
-        bind = binding
-
-        Kiba.job_segment do
-          mod = bind.receiver
-
-          transform Delete::Fields,
-            fields: [mod.collate_fields, :fingerprint,
-              :clean_fingerprint].flatten
-        end
+        Tms::AltNumsCleanupShared.cleanup_job_tags(:objects)
       end
     end
   end
--- lib/kiba/tms/alt_nums_for_refmaster_type_cleanup.rb
+++ lib/kiba/tms/alt_nums_for_refmaster_type_cleanup.rb
@@ -12,44 +12,14 @@ module Kiba
         reader: true
 
       setting :fingerprint_fields,
-        default: %i[number_type correct_type treatment note],
+        default: Tms::AltNumsCleanupShared.clean_fingerprint_fields,
         reader: true
 
       extend Kiba::Extend::Mixins::IterativeCleanup
-
-      setting :orig_fingerprint_fields,
-        default: %i[number_type],
-        reader: true
+      extend Tms::AltNumsCleanupShared
 
       def job_tags
-        %i[alt_nums reference_master cleanup]
-      end
-
-      def worksheet_add_fields
-        %i[correct_type treatment note]
-      end
-
-      def worksheet_field_order
-        %i[number_type correct_type treatment note
-          occurrences occs_with_remarks occs_with_begindate
-          occs_with_enddate
-          example_rec_nums example_values]
-      end
-
-      def fingerprint_flag_ignore_fields
-        %i[number_type]
-      end
-
-      def base_job_cleaned_pre_xforms
-        bind = binding
-
-        Kiba.job_segment do
-          mod = bind.receiver
-
-          transform Fingerprint::Add,
-            target: :fingerprint,
-            fields: mod.orig_fingerprint_fields
-        end
+        Tms::AltNumsCleanupShared.cleanup_job_tags(:reference_master)
       end
     end
   end
