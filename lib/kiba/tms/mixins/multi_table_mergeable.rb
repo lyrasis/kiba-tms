2c5778dcb77c720a0011b7127973d65385137677
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-09-14 11:42:41 -0400

    Nest modules

--- lib/kiba/tms/mixins/multi_table_mergeable.rb
+++ lib/kiba/tms/mixins/multi_table_mergeable.rb
@@ -29,294 +29,298 @@
 #
 # **IF NOT**, manually specify in :for_table_source_job_key setting
 #   in your config module before extending MultiTableMergeable
-module Kiba::Tms::Mixins::MultiTableMergeable
-  include Tms::Mixins::ReportableForTable
-  def self.extended(mod)
-    set_for_table_source_job_key_setting(mod)
-    set_split_on_column_setting(mod)
-    set_target_tables_setting(mod)
-    set_unreportable_for_tables_ok_setting(mod)
-    set_checkable(mod)
-  end
+module Kiba
+  module Tms
+    module Mixins
+      module MultiTableMergeable
+        include Tms::Mixins::ReportableForTable
+        def self.extended(mod)
+          set_for_table_source_job_key_setting(mod)
+          set_split_on_column_setting(mod)
+          set_target_tables_setting(mod)
+          set_unreportable_for_tables_ok_setting(mod)
+          set_checkable(mod)
+        end
 
-  def for?(table)
-    target_tables.any?(table)
-  end
+        def for?(table)
+          target_tables.any?(table)
+        end
 
-  # override manually in module after extending
-  def auto_generate_target_tables
-    true
-  end
-
-  # METHODS USED FOR RUNNING CHECKS
-  #
-  # Reports if there is a target_table with no matching setting
-  #   defined in the config
-  def check_needed_table_transform_settings
-    needed = target_transform_settings_expected.reject do |transform|
-      respond_to?(transform)
-    end
-    return nil if needed.empty?
-
-    "#{name}: add config settings: #{needed.join(", ")}"
-  end
-
-  # Reports if a defined for-target-table transform setting (e.g. a
-  #   `for_*_prepper` method/setting) defined in the config, but no
-  #   value (an actual transform class or :no_xform) has been assigned
-  #   to the setting. These indicate there may be more work to be
-  #   done.
-  def check_undefined_table_transforms
-    undefined =
-      target_transform_settings - target_transform_settings_handled
-    return nil if undefined.empty?
-
-    "#{name}: no transforms defined for: #{undefined.join(", ")}"
-  end
-
-  def check_unreportable_for_tables
-    tables = unreportable_for_tables
-    return nil if tables.empty?
-
-    "#{name}: tables needing :record_num_merge_config defined: "\
-      "#{tables.join(", ")}"
-  end
-
-  # List target tables that do not respond to `:record_num_merge_config`, that
-  #   are not listed in the :unreportable_for_tables_ok setting
-  #
-  # @return [Array<String>] of table names, e.g. "Objects"
-  def unreportable_for_tables
-    target_tables.map { |t| Object.const_get("Tms::#{t}") }
-      .reject { |t| t.respond_to?(:record_num_merge_config) }
-      .map(&:to_s)
-      .map { |val| val.delete_prefix("Kiba::Tms::") }
-      .sort - unreportable_for_tables_ok
-  end
-
-  # All `for_*_prepper` methods/settings defined on extending module
-  #
-  # @return [Array<Symbol>]
-  def target_transform_settings
-    settings
-      .map(&:to_s)
-      .select { |meth| meth.match?(/^for_.*_prepper$/) }
-      .map(&:to_sym)
-  end
-  private :target_transform_settings
-
-  # `for_*_prepper` methods/settings that are explicitly defined with
-  #   an xform class. This list is passed to job definitions to
-  #   configure what xforms each job will run.
-  #
-  # @return [Array<Symbol>]
-  def target_transform_settings_defined_with_xform
-    target_transform_settings.reject do |setting|
-      val = config.values[setting]
-      val.nil? || val == :no_xform
-    end
-  end
-  private :target_transform_settings_defined_with_xform
-
-  # Generates a list of expected `for_*_prepper` method/setting names---one for
-  #   each target table
-  #
-  # @return [Array<Symbol>]
-  def target_transform_settings_expected
-    target_tables.map do |target|
-      tobj = Tms::Table::Obj.new(target)
-      "for_#{tobj.filekey}_prepper".to_sym
-    end
-  end
-  private :target_transform_settings_expected
-
-  # `for_*_prepper` methods/settings defined with actual transform
-  #   classes or :no_xform placeholders to indicate we have analyzed
-  #   the data and found no need for a specific transform. These
-  #   should no longer be flagged as needing attention/work.
-  #
-  # @return [Array<Symbol>]
-  def target_transform_settings_handled
-    target_transform_settings.reject do |setting|
-      config.values[setting].nil?
-    end
-  end
-  private :target_transform_settings_handled
-
-  # METHODS USED FOR AUTO-REGISTERING FOR-TABLE JOBS
-
-  # @param field [Symbol] name of field on which for_tables will be
-  #   split. By default this is `:tablename`. To override, define
-  #   `split_on_column` setting prior to extending this module. See
-  #   {Kiba::Tms::ConRefs} for an example.
-  def register_for_table_jobs(field = split_on_column)
-    key = filekey
-    return unless key
-
-    ns = build_registry_namespace(
-      "#{key}_for",
-      target_tables,
-      field,
-      target_transform_settings_defined_with_xform
-    )
-    Tms.registry.import(ns)
-  end
-
-  def build_registry_namespace(ns_name, targets, field, xforms)
-    bind = binding
-    Dry::Container::Namespace.new(ns_name) do
-      mod = bind.receiver
-      targets.each do |target|
-        targetobj = Tms::Table::Obj.new(target)
-        targetxform = mod.target_xform(xforms, targetobj)
-        params = [mod, ns_name, targetobj, field, targetxform]
-        register targetobj.filekey, mod.send(
-          :target_job_hash, *params
-        )
-      end
-    end
-  end
-
-  def target_xform(xforms, targetobj)
-    sym = "for_#{targetobj.filekey}_prepper".to_sym
-    if xforms.any?(sym)
-      [send(sym)].flatten
-    else
-      []
-    end
-  end
-
-  def for_table_tags(ns_name, targetobj)
-    [
-      ns_name.to_s.delete_suffix("_for").to_sym,
-      targetobj.filekey,
-      :for_table
-    ]
-  end
-
-  def target_job_hash(mod, ns_name, targetobj, field, xforms)
-    key = targetobj.filekey
-    {
-      path: File.join(Tms.datadir, "working", "#{ns_name}_#{key}.csv"),
-      creator: {callee: Tms::Jobs::MultiTableMergeable::ForTable,
-                args: {
-                  source: mod.for_table_source_job_key,
-                  dest: "#{ns_name}__#{key}".to_sym,
-                  targettable: targetobj.tablename,
-                  field: field,
-                  xforms: xforms
-                }},
-      tags: for_table_tags(ns_name, targetobj),
-      lookup_on: mod.lookup_on_field
-    }
-  end
-
-  def lookup_on_field
-    return for_table_lookup_on_field if respond_to?(
-      :for_table_lookup_on_field
-    )
-
-    :recordid
-  end
-
-  # METHODS USED FOR AUTO-CONFIGURING FOR-TABLES
-  def for_table_module_name(jobkey)
-    jobkey.to_s
-      .split(/_+/)
-      .map(&:capitalize)
-      .join
-  end
-
-  def define_for_table_module(target)
-    targetobj = Tms::Table::Obj.new(target)
-    jobkey = "#{table.filekey}_for__#{targetobj.filekey}".to_sym
-
-    moddef = <<~MODDEF
-      module #{for_table_module_name(jobkey)}
-        extend Dry::Configurable
-        module_function
-
-        # Indicates what job output to use as the base for
-        #   non-TMS-table-sourced modules
-        setting :source_job_key, default: :#{jobkey}, reader: true
-        setting :delete_fields, default: [], reader: true
-        setting :empty_fields, default: {}, reader: true
-        extend Tms::Mixins::Tableable
-
-        def used?
+        # override manually in module after extending
+        def auto_generate_target_tables
           true
         end
-      end
-    MODDEF
-    Tms.module_eval(moddef)
-  end
 
-  # METHODS FOR EXTENDING
-  def self.set_checkable(mod)
-    if mod.respond_to?(:checkable)
-      checkable_as_needed(mod)
-    else
-      checkable_from_scratch(mod)
-    end
-  end
-  private_class_method :set_checkable
+        # METHODS USED FOR RUNNING CHECKS
+        #
+        # Reports if there is a target_table with no matching setting
+        #   defined in the config
+        def check_needed_table_transform_settings
+          needed = target_transform_settings_expected.reject do |transform|
+            respond_to?(transform)
+          end
+          return nil if needed.empty?
 
-  def self.set_for_table_source_job_key_setting(mod)
-    return if mod.respond_to?(:for_table_source_job_key)
+          "#{name}: add config settings: #{needed.join(", ")}"
+        end
 
-    str = <<~CFG
-      setting :for_table_source_job_key,
-      default: :prep__#{mod.filekey},
-      reader: true
-    CFG
-    mod.module_eval(str)
-  end
-  private_class_method :set_for_table_source_job_key_setting
+        # Reports if a defined for-target-table transform setting (e.g. a
+        #   `for_*_prepper` method/setting) defined in the config, but no
+        #   value (an actual transform class or :no_xform) has been assigned
+        #   to the setting. These indicate there may be more work to be
+        #   done.
+        def check_undefined_table_transforms
+          undefined =
+            target_transform_settings - target_transform_settings_handled
+          return nil if undefined.empty?
 
-  def self.set_split_on_column_setting(mod)
-    return if mod.respond_to?(:split_on_column)
+          "#{name}: no transforms defined for: #{undefined.join(", ")}"
+        end
 
-    mod.module_eval(
-      "setting :split_on_column, default: :tablename, reader: true",
-      __FILE__,
-      __LINE__ - 2
-    )
-  end
-  private_class_method :set_split_on_column_setting
+        def check_unreportable_for_tables
+          tables = unreportable_for_tables
+          return nil if tables.empty?
 
-  def self.set_target_tables_setting(mod)
-    return if mod.respond_to?(:target_tables)
+          "#{name}: tables needing :record_num_merge_config defined: "\
+            "#{tables.join(", ")}"
+        end
 
-    mod.module_eval(
-      "setting :target_tables, default: [], reader: true",
-      __FILE__,
-      __LINE__ - 2
-    )
-  end
-  private_class_method :set_target_tables_setting
+        # List target tables that do not respond to
+        #   `:record_num_merge_config`, that are not listed in the
+        #   :unreportable_for_tables_ok setting
+        #
+        # @return [Array<String>] of table names, e.g. "Objects"
+        def unreportable_for_tables
+          target_tables.map { |t| Object.const_get("Tms::#{t}") }
+            .reject { |t| t.respond_to?(:record_num_merge_config) }
+            .map(&:to_s)
+            .map { |val| val.delete_prefix("Kiba::Tms::") }
+            .sort - unreportable_for_tables_ok
+        end
 
-  def self.set_unreportable_for_tables_ok_setting(mod)
-    return if mod.respond_to?(:unreportable_for_tables_ok)
+        # All `for_*_prepper` methods/settings defined on extending module
+        #
+        # @return [Array<Symbol>]
+        def target_transform_settings
+          settings
+            .map(&:to_s)
+            .select { |meth| meth.match?(/^for_.*_prepper$/) }
+            .map(&:to_sym)
+        end
+        private :target_transform_settings
 
-    mod.module_eval(
-      "setting :unreportable_for_tables_ok, default: [], reader: true",
-      __FILE__,
-      __LINE__ - 2
-    )
-  end
-  private_class_method :set_unreportable_for_tables_ok_setting
+        # `for_*_prepper` methods/settings that are explicitly defined with
+        #   an xform class. This list is passed to job definitions to
+        #   configure what xforms each job will run.
+        #
+        # @return [Array<Symbol>]
+        def target_transform_settings_defined_with_xform
+          target_transform_settings.reject do |setting|
+            val = config.values[setting]
+            val.nil? || val == :no_xform
+          end
+        end
+        private :target_transform_settings_defined_with_xform
 
-  # METHODS USED BY METHODS USED FOR EXTENDING
-  def self.checkable_as_needed(mod)
-    existing = mod.checkable.dup
-    checkable_from_scratch(mod)
-    combined = mod.checkable.merge(existing)
-    mod.config.checkable = combined
-  end
-  private_class_method :checkable_as_needed
+        # Generates a list of expected `for_*_prepper` method/setting
+        #   names---one for each target table
+        #
+        # @return [Array<Symbol>]
+        def target_transform_settings_expected
+          target_tables.map do |target|
+            tobj = Tms::Table::Obj.new(target)
+            "for_#{tobj.filekey}_prepper".to_sym
+          end
+        end
+        private :target_transform_settings_expected
 
-  def self.checkable_from_scratch(mod)
-    code = %(
+        # `for_*_prepper` methods/settings defined with actual transform
+        #   classes or :no_xform placeholders to indicate we have analyzed
+        #   the data and found no need for a specific transform. These
+        #   should no longer be flagged as needing attention/work.
+        #
+        # @return [Array<Symbol>]
+        def target_transform_settings_handled
+          target_transform_settings.reject do |setting|
+            config.values[setting].nil?
+          end
+        end
+        private :target_transform_settings_handled
+
+        # METHODS USED FOR AUTO-REGISTERING FOR-TABLE JOBS
+
+        # @param field [Symbol] name of field on which for_tables will be
+        #   split. By default this is `:tablename`. To override, define
+        #   `split_on_column` setting prior to extending this module. See
+        #   {Kiba::Tms::ConRefs} for an example.
+        def register_for_table_jobs(field = split_on_column)
+          key = filekey
+          return unless key
+
+          ns = build_registry_namespace(
+            "#{key}_for",
+            target_tables,
+            field,
+            target_transform_settings_defined_with_xform
+          )
+          Tms.registry.import(ns)
+        end
+
+        def build_registry_namespace(ns_name, targets, field, xforms)
+          bind = binding
+          Dry::Container::Namespace.new(ns_name) do
+            mod = bind.receiver
+            targets.each do |target|
+              targetobj = Tms::Table::Obj.new(target)
+              targetxform = mod.target_xform(xforms, targetobj)
+              params = [mod, ns_name, targetobj, field, targetxform]
+              register targetobj.filekey, mod.send(
+                :target_job_hash, *params
+              )
+            end
+          end
+        end
+
+        def target_xform(xforms, targetobj)
+          sym = "for_#{targetobj.filekey}_prepper".to_sym
+          if xforms.any?(sym)
+            [send(sym)].flatten
+          else
+            []
+          end
+        end
+
+        def for_table_tags(ns_name, targetobj)
+          [
+            ns_name.to_s.delete_suffix("_for").to_sym,
+            targetobj.filekey,
+            :for_table
+          ]
+        end
+
+        def target_job_hash(mod, ns_name, targetobj, field, xforms)
+          key = targetobj.filekey
+          {
+            path: File.join(Tms.datadir, "working", "#{ns_name}_#{key}.csv"),
+            creator: {callee: Tms::Jobs::MultiTableMergeable::ForTable,
+                      args: {
+                        source: mod.for_table_source_job_key,
+                        dest: "#{ns_name}__#{key}".to_sym,
+                        targettable: targetobj.tablename,
+                        field: field,
+                        xforms: xforms
+                      }},
+            tags: for_table_tags(ns_name, targetobj),
+            lookup_on: mod.lookup_on_field
+          }
+        end
+
+        def lookup_on_field
+          return for_table_lookup_on_field if respond_to?(
+            :for_table_lookup_on_field
+          )
+
+          :recordid
+        end
+
+        # METHODS USED FOR AUTO-CONFIGURING FOR-TABLES
+        def for_table_module_name(jobkey)
+          jobkey.to_s
+            .split(/_+/)
+            .map(&:capitalize)
+            .join
+        end
+
+        def define_for_table_module(target)
+          targetobj = Tms::Table::Obj.new(target)
+          jobkey = "#{table.filekey}_for__#{targetobj.filekey}".to_sym
+
+          moddef = <<~MODDEF
+            module #{for_table_module_name(jobkey)}
+              extend Dry::Configurable
+              module_function
+
+              # Indicates what job output to use as the base for
+              #   non-TMS-table-sourced modules
+              setting :source_job_key, default: :#{jobkey}, reader: true
+              setting :delete_fields, default: [], reader: true
+              setting :empty_fields, default: {}, reader: true
+              extend Tms::Mixins::Tableable
+
+              def used?
+                true
+              end
+            end
+          MODDEF
+          Tms.module_eval(moddef)
+        end
+
+        # METHODS FOR EXTENDING
+        def self.set_checkable(mod)
+          if mod.respond_to?(:checkable)
+            checkable_as_needed(mod)
+          else
+            checkable_from_scratch(mod)
+          end
+        end
+        private_class_method :set_checkable
+
+        def self.set_for_table_source_job_key_setting(mod)
+          return if mod.respond_to?(:for_table_source_job_key)
+
+          str = <<~CFG
+            setting :for_table_source_job_key,
+            default: :prep__#{mod.filekey},
+            reader: true
+          CFG
+          mod.module_eval(str)
+        end
+        private_class_method :set_for_table_source_job_key_setting
+
+        def self.set_split_on_column_setting(mod)
+          return if mod.respond_to?(:split_on_column)
+
+          mod.module_eval(
+            "setting :split_on_column, default: :tablename, reader: true",
+            __FILE__,
+            __LINE__ - 2
+          )
+        end
+        private_class_method :set_split_on_column_setting
+
+        def self.set_target_tables_setting(mod)
+          return if mod.respond_to?(:target_tables)
+
+          mod.module_eval(
+            "setting :target_tables, default: [], reader: true",
+            __FILE__,
+            __LINE__ - 2
+          )
+        end
+        private_class_method :set_target_tables_setting
+
+        def self.set_unreportable_for_tables_ok_setting(mod)
+          return if mod.respond_to?(:unreportable_for_tables_ok)
+
+          mod.module_eval(
+            "setting :unreportable_for_tables_ok, default: [], reader: true",
+            __FILE__,
+            __LINE__ - 2
+          )
+        end
+        private_class_method :set_unreportable_for_tables_ok_setting
+
+        # METHODS USED BY METHODS USED FOR EXTENDING
+        def self.checkable_as_needed(mod)
+          existing = mod.checkable.dup
+          checkable_from_scratch(mod)
+          combined = mod.checkable.merge(existing)
+          mod.config.checkable = combined
+        end
+        private_class_method :checkable_as_needed
+
+        def self.checkable_from_scratch(mod)
+          code = %(
           setting :checkable,
             default:              {
               needed_table_transform_settings: Proc.new{
@@ -332,7 +336,10 @@ module Kiba::Tms::Mixins::MultiTableMergeable
             reader: true
           ).tr("\n", " ")
 
-    mod.module_eval(code)
+          mod.module_eval(code)
+        end
+        private_class_method :checkable_from_scratch
+      end
+    end
   end
-  private_class_method :checkable_from_scratch
 end
