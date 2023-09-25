3db05c9a06f61a0ef1db1f4f244bbc3de4788ecd
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-09-25 16:32:11 -0400

    Document Dimensions.migration_secondary_unit_vals and set to false

--- lib/kiba/tms/dimensions.rb
+++ lib/kiba/tms/dimensions.rb
@@ -13,8 +13,17 @@ module Kiba
         reader: true
       extend Tms::Mixins::Tableable
 
+      # If primary unit is inches, TMS secondary unit will be cm. If
+      #   client wants to keep both the inch and cm values ***in a
+      #   structured manner*** for *EVERY* dimension recorded, this
+      #   should be set to true. In the real world, it is unlikely
+      #   that any users will consistently enter the metric AND
+      #   imperial values for every recorded dimension when entering
+      #   data manually. For this reason, we default to false, and
+      #   migrate only the values manually recorded in TMS, not the
+      #   derived secondary unit values.
       setting :migrate_secondary_unit_vals,
-        default: true,
+        default: false,
         reader: true
     end
   end
