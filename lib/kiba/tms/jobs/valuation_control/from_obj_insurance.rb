4c5b402bf19f1c645769a0841e654cc00d94868f
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-04-05 20:48:02 -0400

    Implement custom transforms in ValuationControl from ObjInsurance

--- lib/kiba/tms/jobs/valuation_control/from_obj_insurance.rb
+++ lib/kiba/tms/jobs/valuation_control/from_obj_insurance.rb
@@ -12,49 +12,32 @@ module Kiba
 
             Kiba::Extend::Jobs::Job.new(
               files: {
-                source: :prep__obj_insurance,
+                source: :obj_insurance__migrating,
                 destination: :valuation_control__from_obj_insurance
               },
-              transformer: xforms
+              transformer: [
+                config.obj_insurance_pre_xforms,
+                xforms
+              ].compact
             )
           end
 
           def xforms
             Kiba.job_segment do
-              transform do |row|
-                row[:padnote] = nil
-                val = row[:valueisodate]
-                next row if val.blank?
-
-                prefix = "TMS->CS migration note: "
-                case val
-                when /^\d{4}-\d{2}-\d{2}$/
-                  next row
-                when /^\d{4}-\d{2}$/
-                  row[:valueisodate] = "#{val}-01"
-                  row[:padnote] = "#{prefix}Date day value padded to 01"
-                when /^\d{4}$/
-                  row[:valueisodate] = "#{val}-01-01"
-                  row[:padnote] = "#{prefix}Date month/day values padded to "\
-                    "01-01"
-                end
-                row
-              end
-
-              if Tms::ValuationPurposes.used?
-                transform Prepend::ToFieldValue,
-                  field: :valuationpurpose,
-                  value: "Purpose: "
-                transform CombineValues::FromFieldsWithDelimiter,
-                  sources: %i[valuationpurpose valuenotes padnote],
-                  target: :valuenote,
-                  delim: Tms.notedelim
-              else
-                transform CombineValues::FromFieldsWithDelimiter,
-                  sources: %i[valuenotes padnote],
-                  target: :valuenote,
-                  delim: Tms.notedelim
-              end
+              # if Tms::ValuationPurposes.used?
+              #   transform Prepend::ToFieldValue,
+              #     field: :valuationpurpose,
+              #     value: "Purpose: "
+              #   transform CombineValues::FromFieldsWithDelimiter,
+              #     sources: %i[valuationpurpose valuenotes padnote],
+              #     target: :valuenote,
+              #     delim: Tms.notedelim
+              # else
+              #   transform CombineValues::FromFieldsWithDelimiter,
+              #     sources: %i[valuenotes padnote],
+              #     target: :valuenote,
+              #     delim: Tms.notedelim
+              # end
               transform Rename::Fields, fieldmap: {
                 value: :valueamount,
                 currency: :valuecurrency,
--- lib/kiba/tms/valuation_control.rb
+++ lib/kiba/tms/valuation_control.rb
@@ -7,6 +7,10 @@ module Kiba
       extend Tms::Mixins::CsTargetable
 
       module_function
+
+      # @return [nil, Proc] Kiba.job_segment definition of transforms to be run
+      #   at the beginning of :valuation_control__from_obj_insurance
+      setting :obj_insurance_pre_xforms, default: nil, reader: true
     end
   end
 end
